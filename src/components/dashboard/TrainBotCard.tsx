960ab311bfce876892dd9f812c1360dcb7ab93dd
sulemankhan1 <m.sulemankhan@hotmail.com>
2023-08-16 15:38:15 +0500

    alert msg close btn added

--- src/components/dashboard/TrainBotCard.tsx
+++ src/components/dashboard/TrainBotCard.tsx
@@ -86,6 +86,17 @@ const TrainBotCard = () => {
           className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 my-2"
           role="alert"
         >
+          <span className="float-right " onClick={() => setFileError("")}>
+            <svg
+              className="fill-current h-6 w-6 text-gray-700"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <title>Close</title>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+          </span>
           <p>{fileError}</p>
         </div>
       )}
@@ -94,6 +105,17 @@ const TrainBotCard = () => {
           className="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 my-2"
           role="alert"
         >
+          <span className="float-right " onClick={() => setSuccessMsg("")}>
+            <svg
+              className="fill-current h-6 w-6 text-gray-700"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <title>Close</title>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+          </span>
           <p>{successMsg}</p>
         </div>
       )}
