462fa80de1bb124969c417a7cd4896eb6da7845d
sulemankhan1 <m.sulemankhan@hotmail.com>
2023-08-30 19:38:06 +0500

    registration completed

--- src/store/resumeSlice.ts
+++ src/store/resumeSlice.ts
@@ -39,7 +39,7 @@ interface ResumeSlice {
 }
 
 const initialState: ResumeSlice = {
-  uploadedFileName: "1693398706116_1 Resume-M-Suleman-Ibrahim.pdf",
+  uploadedFileName: "",
   // loadingState: "",
   shortName: "",
   name: "",
