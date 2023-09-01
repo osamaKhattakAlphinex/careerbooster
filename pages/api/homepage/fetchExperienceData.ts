225e8ce764b573045f0cd3eff30e268871524083
sulemankhan1 <m.sulemankhan@hotmail.com>
2023-09-01 16:23:11 +0500

    fetching exp, edu apis updated

--- pages/api/homepage/fetchExperienceData.ts
+++ pages/api/homepage/fetchExperienceData.ts
@@ -43,46 +43,49 @@ const handler: NextApiHandler = async (req, res) => {
                 fields: z.object({
                   // id: z.string().describe("random non-repeated id"),
                   jobTitle: z.string().describe("Job Title e.g. Software Dev"),
-                  // company: z
-                  //   .string()
-                  //   .describe("Company Name e.g. Google, Facebook, etc"),
-                  // country: z
-                  //   .string()
-                  //   .describe("Country Name e.g. Nigeria, United States, etc"),
-                  // cityState: z
-                  //   .string()
-                  //   .describe(
-                  //     "City and State e.g. Lagos, Lagos State, New York, New York State, etc"
-                  //   ),
-                  // fromMonth: z
-                  //   .string()
-                  //   .describe("Job Starting Month e.g May, January"),
-                  // fromYear: z
-                  //   .string()
-                  //   .describe("Job Starting Year e.g 2023, 1997"),
-                  // isContinue: z
-                  //   .boolean()
-                  //   .describe("Is Experience continued? e.g true, false"),
-                  // toMonth: z.string().describe("Job Ending Month "),
-                  // toYear: z.string().describe("Job Ending Year"),
-                  // description: z
-                  //   .string()
-                  //   .describe(
-                  //     "Short Description About the Job e.g I did this and that"
-                  //   ),
+                  company: z
+                    .string()
+                    .describe("Company Name e.g. Google, Facebook, etc"),
+                  country: z
+                    .string()
+                    .describe("Country Name e.g. Nigeria, United States, etc"),
+                  cityState: z
+                    .string()
+                    .describe(
+                      "City and State e.g. Lagos, Lagos State, New York, New York State, etc"
+                    ),
+                  fromMonth: z
+                    .string()
+                    .describe("Job Starting Month e.g May, January"),
+                  fromYear: z
+                    .string()
+                    .describe("Job Starting Year e.g 2023, 1997"),
+                  isContinue: z
+                    .boolean()
+                    .describe("Is Experience continued? e.g true, false"),
+                  toMonth: z.string().describe("Job Ending Month "),
+                  toYear: z.string().describe("Job Ending Year"),
+                  description: z
+                    .string()
+                    .describe(
+                      "Short Description About the Job e.g I did this and that"
+                    ),
                 }),
               })
             )
             .describe(
-              "List of all Educations from the provided Data without Skipping any of the Education. Each education has the following fields: id, company, educationLevel, fieldOfStudy, schoolName, schoolLocation, fromMonth, fromYear, isContinue, toMonth, toYear. The Array you return must be sorted by year the latest one on top and oldest on the bottom"
+              "List of all Educations from the provided Data without Skipping any of the Education. Each education has the following fields: id, company, educationLevel, fieldOfStudy, schoolName, schoolLocation, fromMonth, fromYear, isContinue, toMonth, toYear. "
             ),
         })
       );
 
       const formatInstructions = parser.getFormatInstructions();
       const prompt = new PromptTemplate({
-        template:
-          "Answer the users question as best as possible from the provided resume data that you already have about the person.\n{format_instructions}\n{additionalInfo}",
+        template: `Answer the users question as best as possible from the provided resume data that you already have about the person.
+          Make sure the returned array of Experiences is sorted Oldest experiences by month, year should be at the bottom of the array
+          and the newest Experiences should be on very top of the Array.
+          Donot add any extra labels, if there is no data for a field leave it blank.
+          \n{format_instructions}\n{additionalInfo}`,
         inputVariables: ["additionalInfo"],
         partialVariables: { format_instructions: formatInstructions },
       });
