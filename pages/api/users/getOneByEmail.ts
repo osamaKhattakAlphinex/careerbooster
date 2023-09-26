f77b3c5df293fe9a579b05c4c5de251021cea63d
Msulemankhan <m.sulemankhan@hotmail.com>
2023-09-26 12:58:11 +0500

    user package functionality changed

--- pages/api/users/getOneByEmail.ts
+++ pages/api/users/getOneByEmail.ts
@@ -1,7 +1,7 @@
 import { NextApiHandler } from "next";
 import startDB from "@/lib/db";
 import User from "@/db/schemas/User";
-import UserPackage from "@/db/schemas/UserPackage";
+// import UserPackage from "@/db/schemas/UserPackage";
 
 const handler: NextApiHandler = async (req, res) => {
   const email = req?.query?.email;
@@ -11,20 +11,13 @@ const handler: NextApiHandler = async (req, res) => {
 
   await startDB();
 
-  const user = await User.findOne({ email: email })
-    .select("-password")
-    .populate("userPackage");
-  // .populate("userPackage");
+  const user = await User.findOne({ email: email }).select("-password");
+
   if (!user) {
     return res.status(404).json({ error: "User not found" });
   }
 
   return res.status(200).json({ success: true, user });
-  try {
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Internal Server Error", msg: error });
-  }
 };
 
 export default handler;
