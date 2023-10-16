f2f40bcf92afda7083eca5e3da5e1f03cce37630
Msulemankhan <m.sulemankhan@hotmail.com>
2023-10-16 14:23:16 +0500

    updated changes

--- src/components/new-layout/Homepage/PricingSection.tsx
+++ src/components/new-layout/Homepage/PricingSection.tsx
@@ -1,3 +1,5 @@
+import Packages from "@/components/dashboard/checkout/Packages";
+
 const PricingSection = () => {
   return (
     <section className="py-10 py-lg-15">
@@ -19,105 +21,7 @@ const PricingSection = () => {
           </div>
         </div>
         <div className="row g-6 pricing-table">
-          <div
-            className="col-md-6 col-lg-4"
-            data-aos="fade-up-sm"
-            data-aos-delay="50"
-          >
-            <div className="pricing-card p-6 px-lg-10 py-lg-8 rounded-4 h-full bg-">
-              <h1 className="display-3 fw-semibold theme-text mb-0 mt-4  md:!text-4xl text-[24px]">
-                Free Forever
-              </h1>
-              <h3 className="theme-text fw-medium mb-0">$00</h3>
-              {/* <!-- <p className="theme-text lead fw-normal mt-4 mb-0">
-                    A 10X faster way to writing your professional copy
-                  </p> --> */}
-              <a
-                href="pricing-plan.html"
-                className="pricing-btn btn btn-md w-full fs-4 lh-sm mt-9 theme-pricing-btn"
-              >
-                Free Forever
-              </a>
-              <ul className="pricing-list d-flex flex-column gap-5 fs-lg mt-9 mb-0">
-                <li>ATS Scan your Resume</li>
-                <li>Free ATS Optimization</li>
-                <li>Resume Critical Review</li>
-                <li>Rewrite your Resume (Preview Only)</li>
-                <li>Keyword Optimize your LinkedIn (Preview Only)</li>
-              </ul>
-            </div>
-          </div>
-          <div
-            className="col-md-6 col-lg-4"
-            data-aos="fade-up-sm"
-            data-aos-delay="100"
-          >
-            <div className="pricing-card p-6 px-lg-10 py-lg-8 rounded-4 h-full bg-">
-              <h3 className="theme-text fw-medium mb-0">Standard</h3>
-              <h1 className="display-2 fw-semibold theme-text mb-0 mt-4  md:!text-6xl text-[24px]">
-                $99
-              </h1>
-              {/* <!-- <p className="theme-text lead fw-normal mt-4 mb-0">
-                    A 10X faster way to writing your professional copy
-                  </p> --> */}
-              <a
-                href="pricing-plan.html"
-                className="pricing-btn btn btn-md w-full fs-4 lh-sm mt-9 theme-pricing-btn"
-              >
-                Choose Plan
-              </a>
-              <ul className="pricing-list d-flex flex-column gap-5 fs-lg mt-9 mb-0">
-                <li>All features from the Free Package</li>
-                <li>Keyword Optimize your LinkedIn</li>
-                <li>New Executive Level Resume Design</li>
-                <li>Get a Winning Executive Resume</li>
-                <li>Tailor Resume for Each Job (50 Credits)</li>
-                <li>Write Customize Cover Letters (50 Credits)</li>
-                <li>
-                  Write a Personalized Email to the Recruiter (50 Credits)
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div
-            className="col-md-6 col-lg-4"
-            data-aos="fade-up-sm"
-            data-aos-delay="150"
-          >
-            <div className="pricing-card p-6 px-lg-10 py-lg-8 rounded-4 h-full bg-">
-              {/* <!-- <span className="badge text-bg-primary px-4 py-2 rounded-end-0"
-                    >Most Popular</span
-                  > --> */}
-              <h3 className="theme-text fw-medium mb-0">Premium</h3>
-              <h1 className="display-2 fw-semibold theme-text mb-0 mt-4  md:!text-6xl text-[24px]">
-                $500 <span className="text-2xl">/ year</span>
-              </h1>
-              {/* <!-- <p className="theme-text lead fw-normal mt-4 mb-0">
-                    A 10X faster way to writing your professional copy
-                  </p> --> */}
-              <a
-                href="pricing-plan.html"
-                className="pricing-btn btn btn-md w-full fs-4 lh-sm mt-9 theme-pricing-btn"
-              >
-                Choose Plan
-              </a>
-              <ul className="pricing-list d-flex flex-column gap-5 fs-lg mt-9 mb-0">
-                <li>Unlimited seats</li>
-                <li>All features from the Standard Package</li>
-                <li>Unlimited Resume Tailoring</li>
-                <li>Unlimited Custom Cover Letters</li>
-                <li>Unlimited Personalized Emails to Recruiters</li>
-                <li>
-                  Find the HR/Recruiter Email & LinkedIn for Direct Contact for
-                  Job Positions
-                </li>
-                <li>
-                  Generate Monthly Content for your LinkedIn Posts with a Single
-                  Click.
-                </li>
-              </ul>
-            </div>
-          </div>
+          <Packages />
         </div>
       </div>
     </section>
