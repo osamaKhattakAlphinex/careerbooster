3ead2bfe1da6eb98b61c3b771f7847144517ad35
Usama Butt <buttusamarwp@gmail.com>
2023-11-08 19:07:53 +0500

    Registration Flow set

--- src/components/new-layout/linkedin/LinkedInToolMain.tsx
+++ src/components/new-layout/linkedin/LinkedInToolMain.tsx
@@ -1,15 +1,12 @@
 "use client";
 import manyImage from "@/../public/many-image.png";
-import {
-  ArrowDownIcon,
-  refreshIconRotating,
-  uploadIcon,
-} from "@/helpers/iconsProvider";
+import { refreshIconRotating, uploadIcon } from "@/helpers/iconsProvider";
 import Image from "next/image";
 import FAQList from "../Homepage/Faqs";
 import Reviews from "../Homepage/Reviews";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import FileUploadHandler from "@/components/FileUploadHandler";
 
 const LinkedInToolMain = () => {
   const router = useRouter();
@@ -19,47 +16,48 @@ const LinkedInToolMain = () => {
   const [fileUploading, setFileUploading] = useState<boolean>(false);
   const [uploadComplete, setUploadComplete] = useState<boolean>(false);
 
-  const uploadFileToServer = async () => {
-    setFileError("");
-    setFileUploading(true);
-    if (file) {
-      const body = new FormData();
-      body.append("file", file);
+  // const uploadFileToServer = async () => {
+  //   setFileError("");
+  //   setFileUploading(true);
+  //   if (file) {
+  //     const body = new FormData();
+  //     body.append("file", file);
 
-      fetch("/api/fileUpload?type=linkedin-tool", {
-        method: "POST",
-        body,
-      })
-        .then(async (resp: any) => {
-          const res = await resp.json();
-          if (res.success) {
-            const uploadedFileName = res.fileName + "_" + file.name;
-            setFileName(uploadedFileName);
-            // linkedinHeadline(uploadedFileName);
-            // linkedinAbout(uploadedFileName);
+  //     fetch("/api/fileUpload?type=linkedin-tool", {
+  //       method: "POST",
+  //       body,
+  //     })
+  //       .then(async (resp: any) => {
+  //         const res = await resp.json();
+  //         if (res.success) {
+  //           const uploadedFileName = res.fileName + "_" + file.name;
+  //           setFileName(uploadedFileName);
+  //           // linkedinHeadline(uploadedFileName);
+  //           // linkedinAbout(uploadedFileName);
 
-            // router.replace("/welcome?step=1");
-            // router.replace("/register");
-            // setSuccessMsg("File has been uploaded!");
-          } else {
-            setFileError("Something went wrong");
-          }
-        })
-        .catch((error) => {
-          setFileError("Something went wrong");
-        })
-        .finally(() => {
-          setFileUploading(false);
-          setUploadComplete(true);
-        });
-    }
-  };
+  //           // router.replace("/welcome?step=1");
+  //           // router.replace("/register");
+  //           // setSuccessMsg("File has been uploaded!");
+  //         } else {
+  //           setFileError("Something went wrong");
+  //         }
+  //       })
+  //       .catch((error) => {
+  //         setFileError("Something went wrong");
+  //       })
+  //       .finally(() => {
+  //         setFileUploading(false);
+  //         setUploadComplete(true);
+  //       });
+  //   }
+  // };
+
   useEffect(() => {
     if (file && file.type === "application/pdf") {
       //  file exists and is PDF
       setFileError("");
       // upload it to server
-      uploadFileToServer();
+      // uploadFileToServer();
     } else if (file) {
       // if file exists but not PDf
       setFileError("only PDF file is allowed");
@@ -67,57 +65,66 @@ const LinkedInToolMain = () => {
   }, [file]);
   useEffect(() => {
     if (uploadComplete) {
-      router.replace(`/linkedin/result?fileName=${fileName}`);
+      router.replace(`/linkedin/result`);
     }
   }, [uploadComplete, fileName]);
   return (
     <div className="w-full">
       {/* Upload File */}
       <div className="px-36 py-8">
-      <h5 className="text-center w-full font-semibold text-2xl">
-        Keyword-Optimized, Captivating & Under 30 Seconds!
-      </h5>
-      <h3 className="text-center text-5xl font-bold leading-tight mt-3">
-        Free{" "}
-        <span className="text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r to-violet-500 from-fuchsia-500">
-          AI LinkedIn
-        </span>{" "}
-        Summary Generator Achieve Top Rankings in Recruiter Searches and Secure
-        More Interviews!
-      </h3>
-      <h5 className="text-center mt-3 leading-7 font-normal text-2xl ">
-        Simply Upload your Resume or LinkedIn Profile in PDF. Receive results
-        perfectly tailored for you — or we'll compensate you $1000 if we waste
-        your time with irrelevant outcomes.
-      </h5>
-      <div className="flex justify-center mt-11 ">
-        <label className="h-16 w-84 py-3 cursor-pointer  px-6 rounded-xl bg-gradient-to-r to-violet-500 from-fuchsia-500">
-          <input
-            type="file"
-            className="hidden "
-            disabled={fileUploading}
-            onChange={(e) => {
-              if (e.target.files) {
-                setFile(e.target.files[0]);
-              }
-            }}
-          />
-          {fileUploading || uploadComplete ? (
-            <span className="mt-3">{refreshIconRotating}</span>
-          ) : (
-            <div className="flex gap-2 ">
-              <div>{uploadIcon}</div>
-              <div className="text-center ">
-                <p className="m-0 font-semibold text-md [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]">
-                  Upload Resume or LinkedIn in PDF
-                </p>
-                <p className="text-xs">No credit card required</p>
+        <h5 className="text-center w-full font-semibold text-2xl">
+          Keyword-Optimized, Captivating & Under 30 Seconds!
+        </h5>
+        <h3 className="text-center text-5xl font-bold leading-tight mt-3">
+          Free{" "}
+          <span className="text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r to-violet-500 from-fuchsia-500">
+            AI LinkedIn
+          </span>{" "}
+          Summary Generator Achieve Top Rankings in Recruiter Searches and
+          Secure More Interviews!
+        </h3>
+        <h5 className="text-center mt-3 leading-7 font-normal text-2xl ">
+          Simply Upload your Resume or LinkedIn Profile in PDF. Receive results
+          perfectly tailored for you — or we'll compensate you $1000 if we waste
+          your time with irrelevant outcomes.
+        </h5>
+        <div className="flex justify-center mt-11 ">
+          <label className="h-16 w-84 py-3 cursor-pointer  px-6 rounded-xl bg-gradient-to-r to-violet-500 from-fuchsia-500">
+            <input
+              type="file"
+              className="hidden "
+              disabled={fileUploading}
+              onChange={(e) => {
+                if (e.target.files) {
+                  setFile(e.target.files[0]);
+                  setFileName(e.target.files[0].name);
+                }
+              }}
+            />
+            {fileUploading || uploadComplete ? (
+              <span className="mt-3">{refreshIconRotating}</span>
+            ) : (
+              <div className="flex gap-2 ">
+                <div>{uploadIcon}</div>
+                <div className="text-center ">
+                  <p className="m-0 font-semibold text-md [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]">
+                    Upload Resume or LinkedIn in PDF
+                  </p>
+                  <p className="text-xs">No credit card required</p>
+                </div>
               </div>
-            </div>
-          )}
-        </label>
+            )}
+          </label>
+        </div>
       </div>
-      </div>
+      {file !== null && (
+        <FileUploadHandler
+          file={file}
+          // text={text}
+          // setText={setText}
+          // fetchRegistrationDataFromResume={fetchRegistrationDataFromResume}
+        />
+      )}
       {fileError && (
         <div
           className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 my-2 !text-left w-[50%] m-auto"
@@ -128,19 +135,20 @@ const LinkedInToolMain = () => {
       )}
       <div className="flex justify-center  mt-20 mb-20">
         <span className="text-center mt-4 py-2 text-indigo-500 border-indigo-500  rounded-t-2xl rounded-b-2xl border-1">
-          {ArrowDownIcon}
+          {/* {ArrowDownIcon} */}
         </span>
       </div>
       <div className="flex flex-col justify-center">
-      <h3 className="text-center px-10 text-4xl font-semibold leading-normal mt-3">
-        Why Upload a Resume or LinkedIn Profile?
-      </h3>
-      <h5 className="text-center leading-7 font-normal text-xl ">
-        Our AI scans your existing resume or LinkedIn profile to comprehend your
-        career journey, previous work experiences, background, and targeted job
-        position. This allows it to craft a tailormade, keyword-optimized
-        summary and LinkedIn headline perfectly suited for you.
-      </h5>
+        <h3 className="text-center px-10 text-4xl font-semibold leading-normal mt-3">
+          Why Upload a Resume or LinkedIn Profile?
+        </h3>
+        <h5 className="text-center leading-7 font-normal text-xl ">
+          Our AI scans your existing resume or LinkedIn profile to comprehend
+          your career journey, previous work experiences, background, and
+          targeted job position. This allows it to craft a tailormade,
+          keyword-optimized summary and LinkedIn headline perfectly suited for
+          you.
+        </h5>
       </div>
       {/*Vide  Card */}
       <div className="flex mt-20">
@@ -256,9 +264,9 @@ const LinkedInToolMain = () => {
       </div>
       <FAQList />
       <div className="-mx-4">
-      <Reviews />
+        <Reviews />
       </div>
-      
+
       <div className="w-full h-80 flex flex-col justify-center items-center rounded-2xl mt-14 bg-gradient-to-r to-fuchsia-600 from-indigo-500  border-gray-800">
         <div className="w-10/12 flex justify-center items-center flex-col my-4">
           <h3 className="text-4xl text-normal text-center font-bold mt-3">
