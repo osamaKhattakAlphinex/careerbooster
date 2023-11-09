b42c3655be641c892f658287b9c9de96d38fadd5
Msulemankhan <m.sulemankhan@hotmail.com>
2023-11-09 18:45:12 +0500

    max tokens removed

--- pages/api/linkedInBots/linkedinAboutGenerator.ts
+++ pages/api/linkedInBots/linkedinAboutGenerator.ts
@@ -48,7 +48,6 @@ const handler: NextApiHandler = async (req, res) => {
           },
         ],
         temperature: 1,
-        max_tokens: 2000,
       });
       // const resp = await chain4.call({ query: input });
       return res.status(200).json({
