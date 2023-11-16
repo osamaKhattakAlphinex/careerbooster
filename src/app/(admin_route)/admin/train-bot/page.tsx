82205314d5a94310599b8322c89a39e16f73e7fa
Osama Khattak <osamakhattak162@gmail.com>
2023-11-16 16:56:26 +0500

    updated code

--- src/app/(admin_route)/admin/train-bot/page.tsx
+++ src/app/(admin_route)/admin/train-bot/page.tsx
@@ -276,6 +276,31 @@ const TrainRegistrationBotAdminPage = () => {
                 </p>
               </div>
             </div>
+            <div className="flex" onClick={() => setDataType("linkedinTool")}>
+              <div className="flex items-center h-5">
+                <input
+                  name="dataType"
+                  type="radio"
+                  value="linkedinTool"
+                  checked={dataType === "linkedinTool"}
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+              </div>
+              <div className="ml-2 text-sm">
+                <label
+                  htmlFor="helper-radio"
+                  className="font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Linkedin Tool Data sets
+                </label>
+                <p
+                  id="helper-radio-text"
+                  className="text-xs font-normal text-gray-500 dark:text-gray-300"
+                >
+                  Data for Prompts running on Linkedin Tool
+                </p>
+              </div>
+            </div>
           </div>
 
           {/* Tabs */}
