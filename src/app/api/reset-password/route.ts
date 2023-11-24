4e7c54a4378b0e60489bc6cb6f60cf7aea3b8974
usamabutt19 <buttusamarwp@gmail.com>
2023-11-24 13:05:28 +0500

    Contact and reset password functionality added

--- src/app/api/reset-password/route.ts
+++ src/app/api/reset-password/route.ts
@@ -1,9 +1,8 @@
 import startDB from "@/lib/db";
 import User from "@/db/schemas/User";
 import { generateToken } from "@/lib/token";
-import Mailjet from "node-mailjet";
 import { NextResponse } from "next/server";
-
+import nodemailer from "nodemailer";
 export async function POST(req: any) {
   try {
     const body = await req.json();
@@ -23,46 +22,33 @@ export async function POST(req: any) {
 
     // TODO: Send password reset email with token
     const appUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3001";
-    const senderEmail = process.env.SENDER_EMAIL || "info@careerbooster.ai";
-    const receiverEmail =
-      process.env.RECEIVER_EMAIL || "m.sulemankhan@hotmail.com";
+    const senderEmail = process.env.SENDER_EMAIL;
 
-    const mailjet = new Mailjet({
-      apiKey: process.env.SMTP_API_KEY || "your-api-key",
-      apiSecret: process.env.SMTP_SECRET_KEY || "your-api-secret",
+    let transporter = nodemailer.createTransport({
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: senderEmail, // generated brevo user
+        pass: process.env.SMTP_API_KEY, // generated brevo password
+      },
     });
-    const request = mailjet.post("send", { version: "v3.1" }).request({
-      Messages: [
-        {
-          From: {
-            Email: senderEmail,
-            Name: "CareerBooster",
-          },
-          To: [
-            {
-              Email: email,
-            },
-          ],
-          Subject: `Password reset link | CareerBooster.AI`,
-          TextPart: `Here is your password reset link: <a href="${appUrl}/change-password?token=${token}">Reset Password</a>`,
-          HTMLPart: `<h3>You Requested to change your password </h3>
-              <p>Here is your password reset link: <a href="${appUrl}/change-password?token=${token}">Reset Password</a></p>
-                <br />
-                <p>CareerBooster.AI</p>
+    let info = await transporter.sendMail({
+      from: senderEmail, // sender address
+      to: email, // list of receivers
+      subject: `Password reset link | CareerBooster.AI`, // Subject line
+      html: `<h3>You Requested to change your password </h3>
+               <p>Here is your password reset link: <a href="${appUrl}/change-password?token=${token}">Reset Password</a></p>
+                 <br />
+               <p>CareerBooster.AI</p>
               `,
-        },
-      ],
     });
-    request
-      .then((result) => {
-        return NextResponse.json({ success: true }, { status: 200 });
-      })
-      .catch((err) => {
-        return NextResponse.json(
-          { result: "Unable to send email", success: false },
-          { status: 500 }
-        );
-      });
+    if (info.messageId) {
+      return NextResponse.json(
+        { result: "Email sent successfully", success: true },
+        { status: 200 }
+      );
+    }
   } catch (error) {
     console.error(error);
     return NextResponse.json(
---
+++ src/app/sitemap.xml.tsx
@@ -0,0 +1,11 @@
+import { GetServerSideProps } from "next";
+
+export default function Sitemap() {
+  return null;
+}
+
+export const getServerSideProps: GetServerSideProps<{}> = async () => {
+  return {
+    props: {},
+  };
+};
