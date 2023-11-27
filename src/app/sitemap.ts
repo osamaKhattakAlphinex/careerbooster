3c16bba2681d905b29dc173777aa643b19739fa3
usamabutt19 <buttusamarwp@gmail.com>
2023-11-27 16:47:59 +0500

    sitemap.xml is added

---
+++ src/app/sitemap.ts
@@ -0,0 +1,15 @@
+// app/sitemap.js
+
+import { getPages } from "../lib/pages";
+
+const URL = "https://careerbooster.ai";
+
+export default async function sitemap() {
+  const pages = getPages();
+  const pageURLs = pages.map(({ name, route }) => ({
+    url: `${URL}${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  return [...pageURLs];
+}
