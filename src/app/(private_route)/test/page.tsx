36cf306c70372a719b62083ef3635df8346c24b6
usamabutt19 <buttusamarwp@gmail.com>
2023-12-08 13:07:34 +0500

    code updates

--- src/app/(private_route)/test/page.tsx
+++ src/app/(private_route)/test/page.tsx
@@ -5,119 +5,27 @@ import React, { useRef, useState } from "react";
 const Test = () => {
   const [input, setInput] = useState("");
   const [message, setMessage] = useState("");
-  const [audioUrl, setAudioUrl] = useState<any>("");
-  const audioContextRef: any = useRef(null);
-  const sourceRef: any = useRef(null);
   const componentRef: any = useRef(null);
-  function base64ToArrayBuffer(base64: any) {
-    var binaryString = window.atob(base64);
-    var len = binaryString.length;
-    var bytes = new Uint8Array(len);
-    for (var i = 0; i < len; i++) {
-      bytes[i] = binaryString.charCodeAt(i);
-    }
-    return bytes.buffer;
-  }
 
   const handleGenerateSpeech = async () => {
     try {
-      const response = await axios.post(
-        "/api/audioGeneration",
-        { input }
-        // { responseType: "arraybuffer" }
-      );
+      const response = await axios.post("/api/audioGeneration", { input });
 
-      if (response.status === 200) {
-        setMessage("Speech generated successfully");
-        // Play the audio
-        console.log(response.data, typeof response.data);
-        // Convert base64 data to ArrayBuffer
-        const audioData = response.data.result.data; // Assuming the audio data is in base64 format
-        // const arrayBuffer = base64ToArrayBuffer(audioData.data);
-        // debugger;
-        console.log(audioData);
-        // const arrayBuffer = new ArrayBuffer(audioData.data);
-
-        // console.log(arrayBuffer);
-        // // Assuming buffer is the ArrayBuffer you receive from the backend
-
-        // Convert ArrayBuffer to Blob
-        const audioBlob = new Blob(audioData, { type: "audio/mp3" });
-        console.log(audioBlob);
+      if (response) {
+        const audioData = response.data.data;
+        const arrayBufferView = new Uint8Array(audioData);
+        setInput("");
+        const audioBlob = new Blob([arrayBufferView], { type: "audio/mpeg" });
         const url = URL.createObjectURL(audioBlob);
-        console.log(url);
-        // url.play().catch((err) => console.log(err));
-        setAudioUrl(url);
 
-        // // let fileReader = new FileReader();
-        // // let arrayBuffer: any;
-        // // fileReader.onloadend = () => {
-        // //   arrayBuffer = fileReader.result;
-        // //   console.log(arrayBuffer, typeof arrayBuffer);
-        // // };
-
-        // // fileReader.readAsArrayBuffer(audioBlob);
-
-        // const audioBuffer = await audioBlob.arrayBuffer();
-        // console.log(audioBuffer);
-
-        // // Create an audio context
-        // audioContextRef.current = new window.AudioContext();
-        // sourceRef.current = audioContextRef.current.createBufferSource();
-
-        // audioContextRef.current.decodeAudioData(
-        //   audioBuffer,
-        //   (decodedBuffer: any) => {
-        //     // Set the buffer to the source node
-        //     sourceRef.current.buffer = decodedBuffer;
-
-        //     // Connect the source node to the audio context's destination (speakers)
-        //     sourceRef.current.connect(audioContextRef.current.destination);
-
-        //     // Start playing the audio
-        //     sourceRef.current.start(0);
-        //   }
-        // );
-
-        // const audioContext = new window.AudioContext();
-        // // Create an audio buffer source node
-        // const source = audioContext.createBufferSource();
-        // const arrayBuffer = await audioBlob.arrayBuffer();
-        // console.log(arrayBuffer);
-        // // Decode the received buffer
-        // if (arrayBuffer.byteLength) {
-        //   console.log(arrayBuffer.byteLength);
-        //   audioContext.decodeAudioData(arrayBuffer, (decodedBuffer) => {
-        //     debugger;
-        //     // Set the buffer to the source node
-        //     source.buffer = decodedBuffer;
-
-        //     // Connect the source node to the audio context's destination (speakers)
-        //     source.connect(audioContext.destination);
-        //     console.log(source);
-        //     // Start playing the audio
-        //     source.start(0);
-        //   });
-        // }
-
-        // You can play the audio here using HTML audio element or any audio player library
+        componentRef.current.src = url;
+        componentRef.current.load();
+        componentRef.current.play();
       } else {
         setMessage("Speech generation failed");
       }
     } catch (error) {
       setMessage("Something went wrong");
-      // Handle network errors or other exceptions
-    }
-  };
-
-  const handlePlay = () => {
-    const audioElement: any = componentRef.current;
-    if (audioElement && audioElement.src) {
-      audioElement.play().catch((error: any) => {
-        console.error("Error playing audio:", error);
-      });
-    } else {
-      console.error("No valid audio source.");
     }
   };
 
@@ -126,16 +34,7 @@ const Test = () => {
       <textarea value={input} onChange={(e) => setInput(e.target.value)} />
       <button onClick={handleGenerateSpeech}>Generate Speech</button>
       {message && <p>{message}</p>}
-      <audio
-        ref={componentRef}
-        controls
-        src={audioUrl}
-
-        // Specify the type directly with a type assertion
-      >
-        {/* <source src={audioUrl} type="audio/mp3"></source> */}
-      </audio>
-      <button onClick={handlePlay}>Play Audio</button>
+      <audio className="hidden" ref={componentRef} controls />
     </div>
   );
 };
--- src/app/api/audioGeneration/route.ts
+++ src/app/api/audioGeneration/route.ts
@@ -1,46 +1,26 @@
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
-import fs from "fs/promises";
-import path from "path";
+
 export async function POST(req: any) {
   const body = await req.json();
   const { input } = body;
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
-  const speechFile = path.resolve("./public/speech.mp3");
+
   try {
     const mp3 = await openai.audio.speech.create({
       model: "tts-1",
       voice: "alloy",
       input: input,
     });
-    // console.log(mp3);
-    const buffer = Buffer.from(await mp3.arrayBuffer());
-    // console.log(buffer);
-    // console.log(Buffer.isEncoding("utf8"));
-    // console.log(buffer);
-    // const requestHeaders = new Headers(req.headers);
-    //     // await fs.writeFile(speechFile, buffer);
-    //     const response = NextResponse.next({
-    //       request: {
-    //         // New request headers
-    //         headers: requestHeaders,
-    //       },
-    //     });
 
-    //     // Set a new response header x-hello-from-middleware2
-    //     response.headers.set("Content-Type", "audio/mp3");
-    //     return response;
-    return NextResponse.json(
-      {
-        success: true,
-        result: buffer,
-        // mp3,
-        message: "Speech file created successfully",
-      },
-      { status: 200 }
-    );
+    const mp3Buffer = await mp3.arrayBuffer();
+    const buffer = Buffer.from(mp3Buffer);
+
+    const response = NextResponse.json(buffer);
+    response.headers.set("Content-Type", "audio/mpeg");
+    return response;
   } catch (error) {
     console.error("Error generating speech:", error);
     return NextResponse.json(
