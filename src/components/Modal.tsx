c7f3e38bf55ebd6160198ae1f45d3d5414185232
Muhammad Waqas Shaukat <rajawaqas912@gmail.com>
2023-12-11 13:54:26 +0500

    notification modal designed

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,6 @@
 import { crossIcon } from "@/helpers/iconsProvider";
 import React from "react";
+import Notifications from "./Notifications";
 
 interface ModalProps {
   isOpen: boolean;
@@ -10,28 +11,29 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed right-10 top-14 z-50 boverflow-auto bg-black bg-opacity-60">
-      <div className="w-full mx-auto p-4 bg-white rounded-lg shadow-lg">
+    <div className="fixed right-10 top-14 z-50 boverflow-auto bg-[#18181B]  w-1/4 rounded-md">
+      <div className=" w-full  mx-auto p-4  rounded-lg shadow-lg">
         <div className="text-right">
-          <button
+          {/* <button
             className="text-gray-600 hover:text-gray-800"
             onClick={onClose}
           >
             {crossIcon}
-          </button>
+          </button> */}
         </div>
         <div className="mt-4">
-          <h1 className="text-2xl font-semibold">Modal Title</h1>
-          <p className="mt-2">Modal content goes here.</p>
+          {/* <h1 className="text-2xl font-semibold">Modal Title</h1>
+          <p className="mt-2">Modal content goes here.</p> */}
+          <Notifications />
         </div>
-        <div className="mt-6 text-right">
+        {/* <div className="mt-6 text-right">
           <button
             className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
             onClick={onClose}
           >
             Close
           </button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
