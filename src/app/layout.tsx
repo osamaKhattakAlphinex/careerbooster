2edec3d9106d1849510f078b7b5e1e13eae67c03
sulemankhan1 <contact@mubasherkhattak.com>
2023-12-15 18:20:07 +0500

    latest code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,14 @@ export default function RootLayout({
     <ThemeProvider>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
+      </Head>
+      <AuthProvider>
+        <ReduxProvider>
+          {/* <MainLoaderLayer /> */}
+          <Header />
+          <Script
+            dangerouslySetInnerHTML={{
+              __html: `
               (function(h,o,t,j,a,r){
                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
                 h._hjSettings={hjid:3796095,hjsv:6};
@@ -37,13 +42,8 @@ export default function RootLayout({
                 a.appendChild(r);
               })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
             `,
-          }}
-        />
-      </Head>
-      <AuthProvider>
-        <ReduxProvider>
-          {/* <MainLoaderLayer /> */}
-          <Header />
+            }}
+          />
           {children}
           <Footer />
         </ReduxProvider>
