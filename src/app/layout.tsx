639ef2ed15be83607d77e84c8846aab89c7b1d63
sulemankhan1 <m.sulemankhan@hotmail.com>
2023-12-15 16:42:36 +0500

    latest code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import ReduxProvider from "@/components/ReduxProvider";
 import Footer from "@/components/layout/Footer";
 
 import ThemeProvider from "@/components/new-layout/ThemeProvider";
-import MainLoaderLayer from "@/components/new-layout/MainLoaderLayer";
+
 import Head from "next/head";
 
 export const metadata: Metadata = {
@@ -24,20 +24,6 @@ export default function RootLayout({
     <ThemeProvider>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:3796095,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `,
-          }}
-        />
       </Head>
       <AuthProvider>
         <ReduxProvider>
