0ec141a1dc93ae22335e92ca564645d643cbc229
usamabutt19 <m.sulemankhan@hotmail.com>
2023-12-18 11:04:37 +0500

    latest ncode hotjar check

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import AuthProvider from "@/components/AuthProvider";
 import ReduxProvider from "@/components/ReduxProvider";
 import Footer from "@/components/layout/Footer";
-
+//check
 import ThemeProvider from "@/components/new-layout/ThemeProvider";
 
 import Head from "next/head";
