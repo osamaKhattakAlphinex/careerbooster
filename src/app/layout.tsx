59df6de874e40381c5be31ec8e7e319d92037291
usamabutt19 <buttusamarwp@gmail.com>
2023-12-21 10:35:38 +0500

    new code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({
         <ReduxProvider>
           <MainLoaderLayer />
           <UserDataProvider />
-
           {children}
         </ReduxProvider>
       </AuthProvider>
