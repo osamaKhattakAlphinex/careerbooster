ff45b6adf72282ee2b6c4d9195a63a99061b2ab9
usamabutt19 <buttusamarwp@gmail.com>
2023-12-26 13:27:30 +0500

    latest

--- src/app/(private_route)/resume-builder/templates/template/page.tsx
+++ src/app/(private_route)/resume-builder/templates/template/page.tsx
@@ -3,17 +3,25 @@ import { ALL_TEMPLATES } from "@/components/new-dashboard/dashboard/resume-templ
 import { useSearchParams } from "next/navigation";
 import React, { useRef } from "react";
 import RecentResumeCard from "@/components/new-dashboard/dashboard/resume-builder/RecentResumeCard";
+import { useSelector } from "react-redux";
 
 const Template = () => {
   const params = useSearchParams();
+  const resumeData = useSelector((state: any) => state.resume);
+
   const templateId: number = parseInt(params.get("templateId") || "0");
   const componentRef = useRef(null);
   return (
     <div className="ml-[234px] ">
       <RecentResumeCard componentRef={componentRef} />
-      <div ref={componentRef} className="bg-white">
-        {ALL_TEMPLATES[templateId - 1].template({})}
-      </div>
+      {resumeData &&
+        (resumeData?.name ||
+          resumeData?.contact?.email ||
+          resumeData?.summary) && (
+          <div ref={componentRef} className="bg-white">
+            {ALL_TEMPLATES[templateId - 1].template({})}
+          </div>
+        )}
     </div>
   );
 };
