3c33129f7d80fa300fe65aacc3891cd69de0ac30
Muhammad Waqas Shaukat <rajawaqas912@gmail.com>
2023-12-28 10:46:20 +0500

    slider  template

--- src/components/new-dashboard/dashboard/resume-templates/index.tsx
+++ src/components/new-dashboard/dashboard/resume-templates/index.tsx
@@ -12,6 +12,7 @@ import { crownIcon } from "@/helpers/newIconsProviders";
 import { chevronRight } from "@/helpers/iconsProvider";
 
 import { ALL_TEMPLATES } from "@/helpers/templateProvider";
+import TemplateSlider from "./templateSlider";
 export type Template = {
   id: number;
   title: string;
@@ -58,8 +59,6 @@ const Templates = () => {
   const [activeTab, setActiveTab] = useState<Tabs>(tabs[0]);
   const [templates, setTemplates] = useState<Template[]>(ALL_TEMPLATES);
 
-  const userData = useSelector((state: any) => state.userData);
-
   const filterTemplates = () => {
     if (activeTab.tab === "all-templates") {
       setTemplates(ALL_TEMPLATES);
@@ -69,12 +68,6 @@ const Templates = () => {
       );
       setTemplates(_templates);
     }
-  };
-
-  const router = useRouter();
-
-  const handleViewTemplate = (template: Template) => {
-    router.push(`/resume-builder/templates/template?templateId=${template.id}`);
   };
 
   useEffect(() => {
@@ -98,77 +91,7 @@ const Templates = () => {
       </div>
 
       <div className=" text-center "> {activeTab.description}</div>
-
-      <div className="p-4 flex flex-row items-start justify-start gap-6 flex-wrap box-border">
-        <Swiper
-          slidesPerView={5}
-          spaceBetween={10}
-          rewind={true}
-          speed={1200}
-          navigation={true}
-          autoplay={{ delay: 3500, disableOnInteraction: false }}
-          modules={[Autoplay, Navigation]}
-          className=""
-          loop={true}
-          breakpoints={{
-            0: {
-              slidesPerView: 1,
-            },
-            640: {
-              slidesPerView: 1,
-            },
-            768: {
-              slidesPerView: 3,
-            },
-            1080: {
-              slidesPerView: 5,
-            },
-            1280: {
-              slidesPerView: 5,
-            },
-          }}
-        >
-          {templates.map((template, index) => (
-            <SwiperSlide key={`template-${index}`}>
-              <div className="box-border group relative rounded-lg overflow-hidden">
-                {template.category === "premium" && (
-                  <div className="absolute rounded-full right-1 top-1 h-6 w-6 grid place-content-center bg-yellow-600">
-                    {crownIcon}
-                  </div>
-                )}
-                <button
-                  className=""
-                  onClick={() => handleViewTemplate(template)}
-                >
-                  <div className=" object-cover group-hover:grid hidden bg-slate-600/60 text-white  absolute top-0 left-0 h-full w-full rounded-lg  overflow-hidden  place-content-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-8 h-8"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
-                      />
-                    </svg>
-                  </div>
-                  <Image
-                    src={template.preview}
-                    alt={`template-${index}`}
-                    height={350}
-                    width={150}
-                    className="bg-white h-full w-full"
-                  />
-                </button>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      <TemplateSlider templates={templates} />
     </div>
   );
 };
