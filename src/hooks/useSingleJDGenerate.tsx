acba586d40edd14ffc2a8b39d8aef4a4598f504d
nausaltech <buttusamarwp@gmail.com>
2024-01-25 13:47:52 +0500

    working on regeneration fixes

--- src/hooks/useSingleJDGenerate.tsx
+++ src/hooks/useSingleJDGenerate.tsx
@@ -1,119 +1,116 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import useGetUserData from "./useGetUserData";
 import { useDispatch, useSelector } from "react-redux";
-import { setState, setWorkExperience, setWorkExperienceArray } from "@/store/resumeSlice";
+import {
+  setState,
+  setWorkExperience,
+  setWorkExperienceArray,
+} from "@/store/resumeSlice";
 import { WorkExperience, setUserData } from "@/store/userDataSlice";
 import { fetchLIstOfStrings } from "@/helpers/fetchLIstOfStrings";
 import useSaveResumeToDB from "./useSaveToDB";
 import useGetCreditLimits from "./useGetCreditLimits";
 
 const useSingleJDGenerate = (setStreamedJDData: any) => {
-    const { getUserDataIfNotExists } = useGetUserData()
-    const dispatch = useDispatch();
-    const userData = useSelector((state: any) => state.userData);
-    const resumeData = useSelector((state: any) => state.resume);
-    const creditLimits = useSelector((state: any) => state.creditLimits);
-    const { getCreditLimitsIfNotExists } = useGetCreditLimits()
-    let oneWorkExpIndex: number
-    const { saveResumeToDB } = useSaveResumeToDB()
+  const { getUserDataIfNotExists } = useGetUserData();
+  const dispatch = useDispatch();
+  const userData = useSelector((state: any) => state.userData);
+  const resumeData = useSelector((state: any) => state.resume);
+  const creditLimits = useSelector((state: any) => state.creditLimits);
+  const { getCreditLimitsIfNotExists } = useGetCreditLimits();
+  let oneWorkExpIndex: number;
+  const { saveResumeToDB } = useSaveResumeToDB();
 
-    const getOneWorkExperienceNew = async (experience: any) => {
-        setStreamedJDData("");
-        dispatch(setWorkExperience(""));
-        let temp = "";
+  const getOneWorkExperienceNew = async (experience: any) => {
+    // setStreamedJDData("");
+    dispatch(setWorkExperience(""));
+    let temp = "";
 
-        let workExpArray = [...resumeData.workExperienceArray];
-        if (experience && workExpArray.length) {
-            oneWorkExpIndex = workExpArray.findIndex((workExp: any) => JSON.stringify(workExp) === JSON.stringify(experience));
+    let workExpArray = [...resumeData.workExperienceArray];
+    if (experience && workExpArray.length) {
+      oneWorkExpIndex = workExpArray.findIndex(
+        (workExp: any) => JSON.stringify(workExp) === JSON.stringify(experience)
+      );
+    }
+
+    await getUserDataIfNotExists();
+    await getCreditLimitsIfNotExists();
+    const { quantifyingExperience } = resumeData;
+    if (userData.isFetched) {
+      let workExpArrObj: any = {};
+
+      workExpArrObj.title = experience?.title;
+
+      workExpArrObj.cityState = experience?.cityState;
+      workExpArrObj.country = experience?.country;
+      workExpArrObj.company = experience?.company;
+      workExpArrObj.fromMonth = experience?.fromMonth;
+      workExpArrObj.fromYear = experience?.fromYear;
+      workExpArrObj.isContinue = experience?.isContinue;
+      workExpArrObj.toMonth = experience?.toMonth;
+      workExpArrObj.toYear = experience?.toYear;
+
+      let achievementTemp = "";
+      const res: any = await fetch("/api/resumeBots/jdGeneratorSingle", {
+        method: "POST",
+        body: JSON.stringify({
+          quantifyingExperience: quantifyingExperience,
+          experience: experience,
+
+          creditsUsed: creditLimits.resume_individualWorkExperience,
+          trainBotData: {
+            userEmail: userData.email,
+            // fileAddress: userData.files[0].fileName,
+            fileAddress: userData.uploadedResume.fileName,
+          },
+          personName: userData.firstName + " " + userData.lastName,
+          jobTitle: resumeData.state.jobPosition,
+        }),
+      });
+
+      if (res.ok) {
+        const reader = res.body.getReader();
+        while (true) {
+          const { done, value } = await reader.read();
+
+          if (done) {
+            break;
+          }
+
+          const text = new TextDecoder().decode(value);
+          // const text = response.result;
+          setStreamedJDData((prev: any) => prev + text);
+          // temp += text;
+          achievementTemp += text;
         }
+      } else {
+        console.log("in else");
 
-        await getUserDataIfNotExists();
-        await getCreditLimitsIfNotExists()
-        const { quantifyingExperience } = resumeData
-        if (userData.isFetched) {
+        dispatch(setWorkExperienceArray({ workExperienceArray: workExpArray }));
+        dispatch(setState({ name: "resumeLoading", value: false }));
+      }
 
-            let workExpArrObj: any = {};
-            // let html = "";
-            // html += `<h2 style="font-size: 1.3rem; font-weight: bold; line-height: 2rem; ">${experience?.jobTitle}</h2>`;
-            workExpArrObj.title = experience?.title;
+      if (achievementTemp.length > 0) {
+        setStreamedJDData((prev: any) => prev + `</div> <br /> `);
+        temp += `</div> <br /> `;
+        const achivementsArray = fetchLIstOfStrings(achievementTemp);
+        workExpArrObj.achievements = achivementsArray;
 
-            // html += `<h2 style="font-size: 1.1rem; line-height: 1.5rem">
-
-            // ${experience?.fromMonth} ${experience?.fromYear} - ${experience?.isContinue
-            //         ? "Present"
-            //         : experience?.toMonth + " " + experience?.toYear
-            //     } | ${experience?.company} | 
-            // ${experience?.cityState} ${experience?.country}
-            //           </h2>`;
-            // html += `<div>`;
-            workExpArrObj.cityState = experience?.cityState;
-            workExpArrObj.country = experience?.country;
-            workExpArrObj.company = experience?.company;
-            workExpArrObj.fromMonth = experience?.fromMonth;
-            workExpArrObj.fromYear = experience?.fromYear;
-            workExpArrObj.isContinue = experience?.isContinue;
-            workExpArrObj.toMonth = experience?.toMonth;
-            workExpArrObj.toYear = experience?.toYear;
-
-            // temp += html;
-            let achievementTemp = "";
-            // setStreamedJDData((prev: any) => prev + html);
-            const res: any = await fetch("/api/resumeBots/jdGeneratorSingle", {
-                method: "POST",
-                body: JSON.stringify({
-                    quantifyingExperience: quantifyingExperience,
-                    experience: experience,
-
-                    creditsUsed: creditLimits.resume_individualWorkExperience,
-                    trainBotData: {
-                        userEmail: userData.email,
-                        // fileAddress: userData.files[0].fileName,
-                        fileAddress: userData.uploadedResume.fileName,
-                    },
-                    personName: userData.firstName + " " + userData.lastName,
-                    jobTitle: resumeData.state.jobPosition,
-                }),
-            });
-
-            if (res.ok) {
-                const reader = res.body.getReader();
-                while (true) {
-                    const { done, value } = await reader.read();
-
-                    if (done) {
-                        break;
-                    }
-
-                    const text = new TextDecoder().decode(value);
-                    // const text = response.result;
-                    setStreamedJDData((prev: any) => prev + text);
-                    // temp += text;
-                    achievementTemp += text;
-                }
-            }
-
-            setStreamedJDData((prev: any) => prev + `</div> <br /> `);
-            temp += `</div> <br /> `;
-            const achivementsArray = fetchLIstOfStrings(achievementTemp);
-            workExpArrObj.achievements = achivementsArray;
-
-            workExpArray.splice(oneWorkExpIndex, 1, workExpArrObj);
-        }
+        workExpArray.splice(oneWorkExpIndex, 1, workExpArrObj);
         saveResumeToDB({
-            ...resumeData,
-            workExperienceArray: workExpArray,
+          ...resumeData,
+          workExperienceArray: workExpArray,
         });
         dispatch(setWorkExperienceArray({ workExperienceArray: workExpArray }));
         dispatch(setState({ name: "resumeLoading", value: false }));
         // dispatch(setWorkExperience(temp));
         setStreamedJDData("");
-
-
+      }
     }
+  };
 
-
-    return { getOneWorkExperienceNew };
+  return { getOneWorkExperienceNew };
 };
 
 export default useSingleJDGenerate;
