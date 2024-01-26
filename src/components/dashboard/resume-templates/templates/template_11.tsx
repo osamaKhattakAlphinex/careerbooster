e483e7888f2f7e0d405e2533589279786e63ae8a
Rahmat ullah Baheer <baheer224@gmail.com>
2024-01-26 15:39:35 +0500

    updated code push

--- src/components/dashboard/resume-templates/templates/template_11.tsx
+++ src/components/dashboard/resume-templates/templates/template_11.tsx
@@ -269,7 +269,7 @@ const ResumeTemplate11 = () => {
           </div>
 
           <Toolbar regenrateSummary={getSummary}>
-            <div className="xs:min-h-fit min-h-[380px] text-sm  hover:shadow-md border-2 border-transparent hover:border-gray-500 hover:border-dashed">
+            <div className="text-sm  hover:shadow-md border-2 border-transparent hover:border-gray-500 hover:border-dashed">
               <EditableField
                 type="textarea"
                 value={
