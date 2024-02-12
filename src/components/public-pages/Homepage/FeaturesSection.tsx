6014cf64c9bb8592c708ec9eaeefdeac6257c74a
nausaltech <buttusamarwp@gmail.com>
2024-02-12 16:25:59 +0500

    updates

--- src/components/public-pages/Homepage/FeaturesSection.tsx
+++ src/components/public-pages/Homepage/FeaturesSection.tsx
@@ -129,13 +129,11 @@ const FeaturesSection = () => {
             {singleFeatureData.map((item, i) => {
               return (
                 <Slide
+                  key={i}
                   duration={1200}
                   direction={i % 2 === 0 ? "left" : "right"}
                 >
-                  <div
-                    key={i}
-                    className="flex col-span-1 mb-6 px-4 lg:pt-10 md:pt-10 xs:pt-0"
-                  >
+                  <div className="flex col-span-1 mb-6 px-4 lg:pt-10 md:pt-10 xs:pt-0">
                     <div className="flex gap-5">
                       <Fade duration={2000}>
                         <div className="icon w-14 h-14 flex-shrink-0 d-flex align-center justify-center rounded-lg p-2 border-[1px] dark:bg-[#1E1F27] text-[#0000ff9c] bg-[#E5E3F7] dark:text-[#e6f85e] dark:border-white border-[#c7bef9] dark:border-opacity-10 border-opacity-25">
