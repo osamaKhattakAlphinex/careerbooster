90f74d494d0a87a6e5ed1d40736501669183f3a8
nausaltech <contact@mubasherkhattak.com>
2024-02-16 18:41:27 +0500

    model updates

--- src/app/api/resumeBots/getBasicInfo/route.ts
+++ src/app/api/resumeBots/getBasicInfo/route.ts
@@ -190,7 +190,7 @@ export async function POST(req: any) {
               ${promptSummary}`;
 
         const response: any = await openai.chat.completions.create({
-          model: "gpt-3.5-turbo",
+          model: model? model:"gpt-3.5-turbo",
           stream: true,
           messages: [{ role: "user", content: inputPrompt }],
         });
