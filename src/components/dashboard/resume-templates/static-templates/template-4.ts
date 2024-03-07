d56f361e5437cbb125dace5e74e649a39245d839
Muhammad Waqas Shaukat <rajawaqas912@gmail.com>
2024-03-07 16:47:57 +0500

    template-design

--- src/components/dashboard/resume-templates/static-templates/template-4.ts
+++ src/components/dashboard/resume-templates/static-templates/template-4.ts
@@ -3,21 +3,21 @@ const cvHeadings: any = [
     text: "contact",
     section: "phone",
     styles:
-      "font-semibold uppercase text-gray-100 flex items-center text-base py-0.5 w-full border-b-2 border-white",
+      "font-semibold uppercase text-gray-100 flex items-center text-base py-0.5 w-full border-b-2 border-white my-2",
     attributes: [{ "icon-color": "white" }],
   },
   {
     text: "executive summary",
     section: "summary",
     styles:
-      "font-semibold uppercase text-gray-950 flex items-center text-base py-0.5 w-full",
+      "font-semibold uppercase text-gray-950/80 flex items-center text-base py-0.5 w-full",
     attributes: [{ "icon-color": "black" }],
   },
   {
     text: "work experience",
     section: "workExperienceArray",
     styles:
-      "font-semibold uppercase text-gray-950 flex items-center text-base py-0.5 w-full",
+      "font-semibold uppercase text-gray-950/80 flex items-center text-base py-0.5 w-full",
     attributes: [{ "icon-color": "black" }],
   },
   {
@@ -25,14 +25,14 @@ const cvHeadings: any = [
     section: "education",
     attributes: [{ "icon-color": "black" }],
     styles:
-      "font-semibold uppercase text-gray-950 flex items-center text-base py-0.5 w-full",
+      "font-semibold uppercase text-gray-950/80 flex items-center text-base py-0.5 w-full",
   },
   {
     text: "skills",
     section: "primarySkills",
     attributes: [{ "icon-color": "white" }],
     styles:
-      "font-semibold uppercase text-gray-100 flex items-center text-md border-b-2 border-white py-0.5 w-full",
+      "font-semibold uppercase text-gray-100 flex items-center text-md border-b-2 border-white py-0.5 w-full my-2",
   },
 ];
 
@@ -44,74 +44,78 @@ const components: any = {
   },
   name: {
     styles:
-      "text-4xl font-bold flex justify-left text-gray-950 items-center w-full mb-2",
+      "text-2xl font-bold flex justify-left text-gray-950/80 items-center w-full mb-2",
     tag: "span",
   },
   jobTitle: {
     tag: "span",
     styles:
-      "text-base flex justify-left items-center text-gray-950 w-full mb-2",
+      "text-base flex justify-left items-center text-gray-950/80 w-full mb-2",
   },
   contact: {
     styles: "",
     elements: [
       {
         id: "phone",
-        styles: "text-xs mt-2 text-gray-100",
+        styles: "text-xs mt-2 flex break-all before:break-normal text-gray-100",
         tag: "span",
       },
       {
         id: "email",
-        styles: "text-xs mt-2 text-gray-100",
+        styles: "text-xs mt-2 flex break-all before:break-normal text-gray-100",
         tag: "span",
       },
       {
         id: "linkedIn",
-        styles: "text-xs mt-2 text-gray-100",
+        styles: "text-xs mt-2 flex break-all before:break-normal text-gray-100",
         tag: "span",
       },
     ],
   },
   primarySkills: {
-    styles: "text-xs mt-2 text-gray-100 before:content-['\\2022'] before:mr-2",
+    styles:
+      "text-xs flex mt-2 text-gray-100 before:content-['\\2022'] before:mr-2",
     tag: "span",
   },
-  summary: { styles: "text-justify mb-4 text-xs text-gray-950", tag: "span" },
+  summary: {
+    styles: "text-justify mb-4 text-xs text-gray-950/80",
+    tag: "span",
+  },
   workExperienceArray: {
     styles: "",
     elements: [
       {
         id: "title",
-        styles: "text-base text-gray-950 font-bold w-full",
+        styles: "text-base mt-2 text-gray-950/80 font-bold w-full",
         tag: "span",
       },
       {
         styles: "flex gap-1 font-semibold pb-2 text-xs flex-wrap",
         tag: "div",
         container: [
-          { id: "fromMonth", styles: "text-gray-950", tag: "span" },
-          { id: "fromYear", styles: "text-gray-950", tag: "span" },
+          { id: "fromMonth", styles: "text-gray-950/80", tag: "span" },
+          { id: "fromYear", styles: "text-gray-950/80", tag: "span" },
           {
             id: "toMonth",
-            styles: "before:content-['\\268A'] text-gray-950 before:mr-1",
+            styles: "before:content-['\\268A'] text-gray-950/80 before:mr-1",
             tag: "span",
           },
-          { id: "toYear", styles: "text-gray-950", tag: "span" },
+          { id: "toYear", styles: "text-gray-950/80", tag: "span" },
           {
             id: "company",
             styles:
-              "before:content-['\\2758'] before:mr-2 text-gray-950 after:content-['\\2758'] after:ml-2",
+              "before:content-['\\2758'] before:mr-2 text-gray-950/80 after:content-['\\2758'] after:ml-2",
             tag: "span",
           },
-          { id: "cityState", styles: "text-gray-950", tag: "span" },
-          { id: "country", styles: "text-gray-950", tag: "span" },
+          { id: "cityState", styles: "text-gray-950/80", tag: "span" },
+          { id: "country", styles: "text-gray-950/80", tag: "span" },
           // { id: "isContinue", styles: "", tag: "span" },
         ],
       },
       {
         id: "achievements",
         styles:
-          "text-xs flex text-justify pb-1 text-gray-950 before:content-['\\2022'] before:mr-2",
+          "text-xs flex text-justify pb-1 text-gray-950/80 before:content-['\\2022'] before:mr-2",
         tag: "span",
       },
     ],
@@ -163,34 +167,25 @@ const components: any = {
 
 const templateLayout: any = {
   styles: "w-full",
+  attributes: [{ "template-no": "4" }],
   fragment: {
     styles: "flex flex-row bg-white fragment",
     sideBar: {
-      styles: "bg-[#323B4C] w-3/12 flex flex-col justify-start px-6",
+      styles: "bg-[#323B4C] w-3/12 flex flex-col justify-start px-4",
       elements: [
         {
-          heading: false,
-          headingText: "",
           id: "shortName",
         },
         {
-          heading: true,
-          headingText: "Contact",
           id: "phone",
         },
         {
-          heading: true,
-          headingText: "Contact",
           id: "email",
         },
         {
-          heading: true,
-          headingText: "Contact",
-          id: "linkedin",
+          id: "linkedIn",
         },
         {
-          heading: true,
-          headingText: "Skills",
           id: "primarySkills",
         },
       ],
--- src/components/dashboard/resume-templates/static-templates/template-7.ts
+++ src/components/dashboard/resume-templates/static-templates/template-7.ts
@@ -10,31 +10,33 @@ const cvHeadings = [
     section: "summary",
     attributes: [],
     styles:
-      "font-bold mt-4 uppercase text-base text-center text-gray-950 py-0.5",
+      "font-bold mt-4 uppercase text-base text-center text-gray-950/80 border-y-2 border-gray-950/80 py-0.5",
   },
   {
     attributes: [],
     text: "work experience",
     section: "workExperienceArray",
-    styles: "font-bold uppercase text-center text-md text-gray-950 py-0.5",
+    styles:
+      "font-bold uppercase text-center text-md text-gray-950/80 border-y-2 border-gray-950/80 py-0.5",
   },
   {
     attributes: [],
     text: "education",
     section: "education",
     styles:
-      "font-bold text-center mt-2 w-full uppercase text-md text-gray-950 py-0.5",
+      "font-bold text-center mt-2 w-full uppercase text-md text-gray-950/80 border-y-2 border-gray-950/80 py-0.5",
   },
   {
     attributes: [],
     text: "skills",
     section: "primarySkills",
     styles:
-      "font-bold uppercase text-center text-base py-0.5 w-full text-gray-950 before:block",
+      "font-bold uppercase text-center text-base py-0.5 w-full text-gray-950/80 border-y-2 border-gray-950/80",
   },
 ];
 const templateLayout = {
   styles: "w-full",
+  attributes: [{ "template-no": "7" }],
 
   fragment: {
     styles: "flex flex-col bg-white fragment py-5 relative",
@@ -68,7 +70,7 @@ const templateLayout = {
       ],
     },
     summary: {
-      styles: "bg-red text-black text-junstify px-8",
+      styles: "bg-red text-black text-junstify px-8 pt-8",
       elements: [
         {
           id: "summary",
@@ -77,7 +79,7 @@ const templateLayout = {
     },
     skills: {
       styles:
-        "bg-red text-black w-full flex-1 gap-2 flex flex-row flex-wrap justify-start items-start px-8 my-6",
+        "text-black w-full flex-1 gap-2 flex flex-row flex-wrap justify-start items-start px-8 my-6",
       elements: [
         {
           id: "primarySkills",
@@ -111,12 +113,12 @@ const components = {
     tag: "span",
   },
   name: {
-    styles: "text-4xl font-bold flex text-gray-950 items-center w-full mx-6",
+    styles: "text-2xl font-bold flex text-gray-950/80 items-center w-full mx-6",
     tag: "span",
   },
   jobTitle: {
     tag: "span",
-    styles: "text-lg flex text-gray-950 w-full mb-2 px-6",
+    styles: "text-base flex text-gray-950/80 w-full mb-2 px-6",
   },
   contact: {
     styles: "w-full",
@@ -124,64 +126,64 @@ const components = {
       {
         id: "phone",
         styles:
-          "text-xs text-gray-950 mt-2 break-all before:break-normal before:content-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGZpbGw9Im5vbmUiIHZpZXdCb3g9IjAgMCAyNCAyNCIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZT0iY3VycmVudENvbG9yIiBjbGFzcz0idy02IGgtNiI+CiAgPHBhdGggc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBkPSJNMi4yNSA2Ljc1YzAgOC4yODQgNi43MTYgMTUgMTUgMTVoMi4yNWEyLjI1IDIuMjUgMCAwIDAgMi4yNS0yLjI1di0xLjM3MmMwLS41MTYtLjM1MS0uOTY2LS44NTItMS4wOTFsLTQuNDIzLTEuMTA2Yy0uNDQtLjExLS45MDIuMDU1LTEuMTczLjQxN2wtLjk3IDEuMjkzYy0uMjgyLjM3Ni0uNzY5LjU0Mi0xLjIxLjM4YTEyLjAzNSAxMi4wMzUgMCAwIDEtNy4xNDMtNy4xNDNjLS4xNjItLjQ0MS4wMDQtLjkyOC4zOC0xLjIxbDEuMjkzLS45N2MuMzYzLS4yNzEuNTI3LS43MzQuNDE3LTEuMTczTDYuOTYzIDMuMTAyYTEuMTI1IDEuMTI1IDAgMCAwLTEuMDkxLS44NTJINC41QTIuMjUgMi4yNSAwIDAgMCAyLjI1IDQuNXYyLjI1WiIgLz4KPC9zdmc+Cg==')] before:w-4 before:h-4 flex before:mr-2",
+          "text-xs text-gray-950/80 mt-2 break-all before:break-normal before:content-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGZpbGw9Im5vbmUiIHZpZXdCb3g9IjAgMCAyNCAyNCIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZT0iY3VycmVudENvbG9yIiBjbGFzcz0idy02IGgtNiI+CiAgPHBhdGggc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBkPSJNMi4yNSA2Ljc1YzAgOC4yODQgNi43MTYgMTUgMTUgMTVoMi4yNWEyLjI1IDIuMjUgMCAwIDAgMi4yNS0yLjI1di0xLjM3MmMwLS41MTYtLjM1MS0uOTY2LS44NTItMS4wOTFsLTQuNDIzLTEuMTA2Yy0uNDQtLjExLS45MDIuMDU1LTEuMTczLjQxN2wtLjk3IDEuMjkzYy0uMjgyLjM3Ni0uNzY5LjU0Mi0xLjIxLjM4YTEyLjAzNSAxMi4wMzUgMCAwIDEtNy4xNDMtNy4xNDNjLS4xNjItLjQ0MS4wMDQtLjkyOC4zOC0xLjIxbDEuMjkzLS45N2MuMzYzLS4yNzEuNTI3LS43MzQuNDE3LTEuMTczTDYuOTYzIDMuMTAyYTEuMTI1IDEuMTI1IDAgMCAwLTEuMDkxLS44NTJINC41QTIuMjUgMi4yNSAwIDAgMCAyLjI1IDQuNXYyLjI1WiIgLz4KPC9zdmc+Cg==')] before:w-4 before:h-4 flex before:mr-2",
         tag: "span",
       },
       {
         id: "email",
         styles:
-          "text-xs break-all before:break-normal text-gray-950 mt-2 before:w-4 before:h-4 before:content-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGZpbGw9Im5vbmUiIHZpZXdCb3g9IjAgMCAyNCAyNCIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZT0iY3VycmVudENvbG9yIiBjbGFzcz0idy02IGgtNiI+CiAgPHBhdGggc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBkPSJNMjEuNzUgNi43NXYxMC41YTIuMjUgMi4yNSAwIDAgMS0yLjI1IDIuMjVoLTE1YTIuMjUgMi4yNSAwIDAgMS0yLjI1LTIuMjVWNi43NW0xOS41IDBBMi4yNSAyLjI1IDAgMCAwIDE5LjUgNC41aC0xNWEyLjI1IDIuMjUgMCAwIDAtMi4yNSAyLjI1bTE5LjUgMHYuMjQzYTIuMjUgMi4yNSAwIDAgMS0xLjA3IDEuOTE2bC03LjUgNC42MTVhMi4yNSAyLjI1IDAgMCAxLTIuMzYgMEwzLjMyIDguOTFhMi4yNSAyLjI1IDAgMCAxLTEuMDctMS45MTZWNi43NSIgLz4KPC9zdmc+Cg==')] flex before:mr-2",
+          "text-xs break-all before:break-normal text-gray-950/80 mt-2 before:w-4 before:h-4 before:content-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGZpbGw9Im5vbmUiIHZpZXdCb3g9IjAgMCAyNCAyNCIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZT0iY3VycmVudENvbG9yIiBjbGFzcz0idy02IGgtNiI+CiAgPHBhdGggc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBkPSJNMjEuNzUgNi43NXYxMC41YTIuMjUgMi4yNSAwIDAgMS0yLjI1IDIuMjVoLTE1YTIuMjUgMi4yNSAwIDAgMS0yLjI1LTIuMjVWNi43NW0xOS41IDBBMi4yNSAyLjI1IDAgMCAwIDE5LjUgNC41aC0xNWEyLjI1IDIuMjUgMCAwIDAtMi4yNSAyLjI1bTE5LjUgMHYuMjQzYTIuMjUgMi4yNSAwIDAgMS0xLjA3IDEuOTE2bC03LjUgNC42MTVhMi4yNSAyLjI1IDAgMCAxLTIuMzYgMEwzLjMyIDguOTFhMi4yNSAyLjI1IDAgMCAxLTEuMDctMS45MTZWNi43NSIgLz4KPC9zdmc+Cg==')] flex before:mr-2",
         tag: "span",
       },
       {
         id: "linkedIn",
         styles:
-          "text-xs break-all before:break-normal before:p-1 text-gray-950 mt-2 before:w-4 before:h-4 before:rounded-sm before:content-['in'] before:border-2 before:border-gray-700 before:flex before:justify-center before:items-center flex before:mr-2",
+          "text-xs break-all before:break-normal before:p-1 text-gray-950/80 mt-2 before:w-4 before:h-4 before:font-semibold flex before:text-xs before:content-['in'] before:border-[1.5px] before:border-[#333333] before:flex before:justify-center before:rounded-sm before:items-center flex before:mr-2",
         tag: "span",
       },
     ],
   },
   primarySkills: {
     styles:
-      "text-xs inline-block w-[32%] before:content-['\\2022'] text-gray-950 flex before:mr-2",
+      "text-xs inline-block w-[32%] before:content-['\\2022'] text-gray-950/80 flex before:mr-2",
     tag: "span",
   },
-  summary: { styles: "text-center mb-4 text-xs text-gray-950", tag: "span" },
+  summary: { styles: "text-center mb-4 text-xs text-gray-950/80", tag: "span" },
   workExperienceArray: {
     styles: "my-2",
     elements: [
       {
         id: "title",
-        styles: "text-lg text-gray-950 font-bold",
+        styles: "text-base mt-2 text-gray-950/80 font-bold",
         tag: "span",
       },
       {
-        styles: "flex gap-1 font-semibold pb-2 text-sm flex-wrap",
+        styles: "flex gap-1 font-semibold pb-2 text-xs flex-wrap",
         tag: "div",
         container: [
-          { id: "fromMonth", styles: "text-gray-950", tag: "span" },
-          { id: "fromYear", styles: "text-gray-950", tag: "span" },
+          { id: "fromMonth", styles: "text-gray-950/80", tag: "span" },
+          { id: "fromYear", styles: "text-gray-950/80", tag: "span" },
           {
             id: "toMonth",
-            styles: "before:content-['\\268A'] text-gray-950 before:mr-1",
+            styles: "before:content-['\\268A'] text-gray-950/80 before:mr-1",
             tag: "span",
           },
-          { id: "toYear", styles: "text-gray-950", tag: "span" },
+          { id: "toYear", styles: "text-gray-950/80", tag: "span" },
           {
             id: "company",
             styles:
-              "before:content-['\\2758'] before:mr-2 text-gray-950 after:content-['\\2758'] after:ml-2",
+              "before:content-['\\2758'] before:mr-2 text-gray-950/80 after:content-['\\2758'] after:ml-2",
             tag: "span",
           },
-          { id: "cityState", styles: "text-gray-950", tag: "span" },
-          { id: "country", styles: "text-gray-950", tag: "span" },
+          { id: "cityState", styles: "text-gray-950/80", tag: "span" },
+          { id: "country", styles: "text-gray-950/80", tag: "span" },
           // { id: "isContinue", styles: "", tag: "span" },
         ],
       },
       {
         id: "achievements",
         styles:
-          "text-sm text-justify text-gray-950 flex pb-1 before:content-['\\2022'] before:mr-2",
+          "text-xs text-justify text-gray-950/80 flex pb-1 before:content-['\\2022'] before:mr-2",
         tag: "span",
       },
     ],
@@ -192,7 +194,7 @@ const components = {
     elements: [
       {
         tag: "div",
-        styles: "flex flex-col w-[30%] p-4 rounded-md",
+        styles: "flex flex-col w-[30%] p-2 rounded-md",
         container: [
           {
             id: "educationLevel",
