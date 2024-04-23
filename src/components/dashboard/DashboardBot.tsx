f0eb57d9155a955dc9f6df77d99a7f569a6e34c9
Muhammad Waqas Shaukat <rajawaqas912@gmail.com>
2024-04-23 17:47:06 +0500

    warning removed

--- src/components/dashboard/DashboardBot.tsx
+++ src/components/dashboard/DashboardBot.tsx
@@ -66,13 +66,13 @@ const Tooltip: React.FC<TooltipProps> = ({
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-6 h-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
             />
           </svg>
@@ -245,7 +245,7 @@ const DashboardBot = () => {
         if (isAudioPlaying) {
           componentRef.current.pause();
           setShowBot(false);
-          if(!userData.tours?.dashboard) {
+          if (!userData.tours?.dashboard) {
             updateAndSaveTourStatus({ dashboard: true });
           }
           localStorage.setItem("botHidden", "true");
@@ -378,7 +378,7 @@ const DashboardBot = () => {
             e.stopPropagation();
             setShowBot(false);
             localStorage.setItem("botHidden", "true");
-            if(!userData.tours?.dashboard) {
+            if (!userData.tours?.dashboard) {
               updateAndSaveTourStatus({ dashboard: true });
             }
             setIsGif(false);
--- src/components/dashboard/TourBot.tsx
+++ src/components/dashboard/TourBot.tsx
@@ -30,13 +30,13 @@ const Tooltip: React.FC<TooltipProps> = ({ text, children, audioPlayed }) => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-6 h-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
             />
           </svg>
@@ -124,7 +124,7 @@ const TourBot = ({ config, setOutOfCredits }: any) => {
         if (isAudioPlaying) {
           audioPlayerRef.current.pause();
           setShowBot(false);
-          if(!userData?.tours[config.name]) {
+          if (!userData?.tours[config.name]) {
             updateAndSaveTourStatus({ [config.name]: true });
           }
           localStorage.setItem("botHidden", "true");
@@ -245,7 +245,7 @@ const TourBot = ({ config, setOutOfCredits }: any) => {
           onClick={(e) => {
             e.stopPropagation();
             setShowBot(false);
-            if(!userData?.tours[config.name]) {
+            if (!userData?.tours[config.name]) {
               updateAndSaveTourStatus({ [config.name]: true });
             }
             localStorage.setItem("botHidden", "true");
