9a8a5133cce6606709c6cd2edfd95c9798264e29
nausaltech <contact@mubasherkhattak.com>
2024-05-07 13:37:25 +0500

    latest

---
+++ src/db/schemas/Job.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const JobSchema = new Schema(
+  {
+    jobTitle: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    employer: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "JobCategory",
+      required: true,
+    },
+    jobDescription: {
+      type: String,
+      required: true,
+    },
+    addedByUserId: {
+      type: mongoose.SchemaTypes.ObjectId,
+      required: true,
+    },
+    link: String,
+    skills: [],
+    rejectMsg: String,
+    noOfProposals: {
+      type: Number,
+      default: 0,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "active", "started", "rejected", "completed"],
+      required: true,
+    },
+    featured: {
+      type: Number,
+      default: 0,
+    },
+    author: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Job = mongoose.models.Job || mongoose.model("Job", JobSchema);
+export default Job;
