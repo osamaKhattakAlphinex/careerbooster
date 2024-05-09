5e5e451f7726ad016a91b50ac0e7548e7343dbb9
Muhammad Waqas Shaukat <rajawaqas912@gmail.com>
2024-05-09 10:58:40 +0500

    conflict resolved

--- src/app/(deo_route)/deo/page.tsx
+++ src/app/(deo_route)/deo/page.tsx
@@ -39,7 +39,7 @@ const Jobs = () => {
   const [records, setRecords] = useState<[] | any>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [open, setOpen] = useState<boolean>(false);
-  const [currentRecord, setCurrentRecord] = useState<any>(null)
+  const [currentRecord, setCurrentRecord] = useState<any>(null);
   const [deo, setDeo] = useState<any>({});
   const { data: session } = useSession();
 
@@ -77,14 +77,9 @@ const Jobs = () => {
 
   const viewMessageHandler = (rec: any) => {};
   const editJobHandler = (rec: any) => {
-    setCurrentRecord(rec)
-    setOpen(true)
+    setCurrentRecord(rec);
+    setOpen(true);
   };
-<<<<<<< HEAD
-  const feturedHandler = (e: any, rec: any) => {
-    debugger;
-    console.log(e);
-=======
   const statusHandler = (rec: any, newStatus: string) => {
     axios
       .put(`/api/deo?jobId=${rec._id}`, {
@@ -118,7 +113,6 @@ const Jobs = () => {
       .finally(() => {
         setLoading(false);
       });
->>>>>>> e1f1bada11ca1a9404a6f2a9874434c105f0b4b3
   };
 
   const deleteJobHandler = (rec: any) => {
@@ -143,15 +137,6 @@ const Jobs = () => {
   const columns = [
     columnHelper.accessor("jobTitle", {
       id: "jobTitle",
-<<<<<<< HEAD
-      header: () => "Job Title",
-      // cell: (info) => info.renderValue(),
-    }),
-    columnHelper.accessor("location", {
-      id: "location",
-      header: () => "Location",
-      // cell: (info) => info.renderValue(),
-=======
       header: () => "jobTitle",
       cell: (info: any) => info.renderValue(),
     }),
@@ -159,16 +144,11 @@ const Jobs = () => {
       id: "location",
       header: () => "location",
       cell: (info: any) => info.renderValue(),
->>>>>>> e1f1bada11ca1a9404a6f2a9874434c105f0b4b3
     }),
     columnHelper.accessor("employer", {
       id: "employer",
       header: () => "employer",
-<<<<<<< HEAD
-      // cell: (info) => info.renderValue(),
-=======
       cell: (info: any) => info.renderValue(),
->>>>>>> e1f1bada11ca1a9404a6f2a9874434c105f0b4b3
     }),
 
     columnHelper.accessor("link", {
@@ -232,51 +212,6 @@ const Jobs = () => {
       header: () => "featured",
       cell: (info) => {
         return (
-<<<<<<< HEAD
-          <label
-            htmlFor="featured-job"
-            className="grid p-1 bg-white place-content-center"
-          >
-            {info.renderValue() === 1 ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="#FFD700"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="white"
-                className="w-6 h-6 pointer-events-none"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="#FFD700"
-                className="w-6 h-6 pointer-events-none"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
-                />
-              </svg>
-            )}
-            <input
-              className="sr-only"
-              type="radio"
-              id="featured-job"
-              onChange={(e) => feturedHandler(e, info)}
-              checked={info.renderValue() === 1 ? true : false}
-            />
-          </label>
-=======
           <input
             type="checkbox"
             onChange={(e) =>
@@ -284,7 +219,6 @@ const Jobs = () => {
             }
             checked={info.getValue() === 1}
           />
->>>>>>> e1f1bada11ca1a9404a6f2a9874434c105f0b4b3
         );
       },
     }),
@@ -330,11 +264,7 @@ const Jobs = () => {
   }, [session]);
   useEffect(() => {
     fetchRecords();
-<<<<<<< HEAD
-  }, [deo]);
-=======
   }, [deo, open]);
->>>>>>> e1f1bada11ca1a9404a6f2a9874434c105f0b4b3
 
   return (
     <>
@@ -352,7 +282,7 @@ const Jobs = () => {
             <button
               onClick={() => {
                 setCurrentRecord(null);
-                setOpen(true)
+                setOpen(true);
               }}
               className="px-4 py-2 text-sm font-semibold text-gray-500 border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
@@ -360,7 +290,13 @@ const Jobs = () => {
             </button>
           </div>
         </div>
-        {open ? <JobForm setOpen={setOpen} deoId={deo._id} singleRec={currentRecord} /> : null}
+        {open ? (
+          <JobForm
+            setOpen={setOpen}
+            deoId={deo._id}
+            singleRec={currentRecord}
+          />
+        ) : null}
         <div className="w-full mt-4 overflow-x-auto">
           {records?.length > 0 ? (
             <DataTable
