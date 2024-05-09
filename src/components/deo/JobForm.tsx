f0f4f65c8e132571b7d91d5f63084b10d6b25902
Muhammad Waqas Shaukat <rajawaqas912@gmail.com>
2024-05-09 12:38:32 +0500

    form design fixes

--- src/components/deo/JobForm.tsx
+++ src/components/deo/JobForm.tsx
@@ -3,16 +3,16 @@ import JobFormInput from "./JobFormInput";
 import { crossIcon } from "@/helpers/iconsProvider";
 type Props = {
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  deoId:any
-  singleRec?: any
+  deoId: any;
+  singleRec?: any;
 };
-const JobForm = ({ setOpen,deoId, singleRec }: Props) => {
+const JobForm = ({ setOpen, deoId, singleRec }: Props) => {
   return (
-    <div className="absolute inset-0 w-full h-full bg-black/90 z-30">
-      <div className="w-full h-full flex justify-center mt-12">
+    <div className="absolute inset-0 z-30 w-full h-full bg-black/90">
+      <div className="flex justify-center w-full h-full mt-12">
         <JobFormInput deoId={deoId} setOpen={setOpen} singleRec={singleRec} />
         <div
-          className="absolute right-28 top-7 cursor-pointer"
+          className="absolute cursor-pointer right-28 top-7"
           onClick={() => setOpen(false)}
         >
           {crossIcon}
