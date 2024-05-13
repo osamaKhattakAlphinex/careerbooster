9255d896184dd6f3edde32dc11782aeb00d39f26
nausaltech <buttusamarwp@gmail.com>
2024-05-13 09:02:32 +0500

    fixes in models

--- src/app/api/homepage/fetchRegistrationDataForHomepage/route.ts
+++ src/app/api/homepage/fetchRegistrationDataForHomepage/route.ts
@@ -39,7 +39,7 @@ export async function POST(req: any) {
 
         // const resp = await model.call(input);
         const response = await openai.chat.completions.create({
-          model: "gpt-4-turbo", // v2
+          model: "gpt-3.5-turbo", // v2
           messages: [
             {
               role: "user",
