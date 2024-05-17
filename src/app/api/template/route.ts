2abc7b74246acc582f95ee6e6413601d4db6d857
nausaltech <contact@mubasherkhattak.com>
2024-05-17 11:44:01 +0500

    another check

--- src/app/api/template/route.ts
+++ src/app/api/template/route.ts
@@ -2,6 +2,9 @@ import { NextResponse } from "next/server";
 import puppeteer from "puppeteer-core";
 import puppeteerDev from "puppeteer";
 import chromium from "@sparticuz/chromium";
+export const maxDuration = 300; // This function can run for a maximum of 5 seconds
+export const dynamic = "force-dynamic";
+
 export async function POST(req: any) {
   // try {
   const data = await req.json();
