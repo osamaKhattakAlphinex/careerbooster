b3e33a22cbad618759aa6c6c9ac7094b29044893
Muhammad Waqas Shaukat <rajawaqas912@gmail.com>
2024-05-22 15:51:20 +0500

    service form

--- src/app/(admin_route)/admin/sales/page.tsx
+++ src/app/(admin_route)/admin/sales/page.tsx
@@ -3,8 +3,10 @@ import DataTable, { TableAction } from "@/components/admin/DataTable";
 import PaymentsDecryptionModal from "@/components/admin/payments/paymentsDecryptionModal";
 import { useAppContext } from "@/context/AppContext";
 import { getFormattedDate } from "@/helpers/getFormattedDateTime";
+import { boltIcon } from "@/helpers/iconsProvider";
 import { createColumnHelper } from "@tanstack/react-table";
 import axios from "axios";
+import { stat } from "fs";
 import React, { useEffect, useRef, useState } from "react";
 
 type Sale = {
@@ -14,6 +16,50 @@ type Sale = {
   status: string;
   phone: string;
   createdAt: string;
+};
+
+const ChangeStatus = ({ sale }: any) => {
+  const [status, setStatus] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (sale.status === "pending") {
+      setStatus(false);
+    } else {
+      setStatus(true);
+    }
+  }, []);
+
+  return (
+    <label
+      htmlFor="status"
+      className={`${status ? "line-through " : " "} flex flex-row gap-2 `}
+    >
+      <input
+        id="status"
+        type="checkbox"
+        checked={status}
+        className=""
+        onChange={(e) => {
+          const newStatus = e.target.checked ? "completed" : "pending";
+          const updatedSale = { ...sale, status: newStatus };
+
+          axios
+            .put(`/api/sales/${sale.id}`, updatedSale)
+            .then((res: any) => {
+              if (res.data.success) {
+                const sales = res.data.result;
+                console.log(sales);
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            })
+            .finally(() => {});
+        }}
+      />{" "}
+      Completed
+    </label>
+  );
 };
 
 const Sales = () => {
@@ -60,24 +106,32 @@ const Sales = () => {
     }),
   ];
 
-  // const actions: TableAction[] = [
-  // {
-  //   name: "update",
-  //   type: "handler",
-  //   element: (credit: any) => handleCreditUpdate(credit),
-  //   styles:
-  //     "whitespace-nowrap px-3 py-2 text-xs font-medium text-center text-white bg-green-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 no-underline",
-  //   icon: EditIcon,
-  // },
-  // {
-  //   name: "delete",
-  //   type: "handler",
-  //   element: (credit: Credit) => handleOpenConfirmationModal(credit),
-  //   styles:
-  //     "whitespace-nowrap px-3 py-2 text-xs font-medium text-center text-white bg-rose-700 rounded-lg hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-rose-300 dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800 no-underline",
-  //   icon: deleteIcon,
-  // },
-  // ];
+  const actions: TableAction[] = [
+    {
+      name: "Change Status",
+      type: "component",
+      element: (sale: any) => <ChangeStatus sale={sale} />,
+      styles: "",
+      icon: boltIcon,
+    },
+
+    // {
+    //   name: "update",
+    //   type: "handler",
+    //   element: (credit: any) => handleCreditUpdate(credit),
+    //   styles:
+    //     "whitespace-nowrap px-3 py-2 text-xs font-medium text-center text-white bg-green-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 no-underline",
+    //   icon: EditIcon,
+    // },
+    // {
+    //   name: "delete",
+    //   type: "handler",
+    //   element: (credit: Credit) => handleOpenConfirmationModal(credit),
+    //   styles:
+    //     "whitespace-nowrap px-3 py-2 text-xs font-medium text-center text-white bg-rose-700 rounded-lg hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-rose-300 dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800 no-underline",
+    //   icon: deleteIcon,
+    // },
+  ];
 
   const fetchSales = async () => {
     setLoading(true);
@@ -122,8 +176,7 @@ const Sales = () => {
             columns={columns}
             data={sales}
             source="sales"
-            actions={[]}
-            conditionalTableAction={[]}
+            actions={actions}
           />
         </div>
       </div>
