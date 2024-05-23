46fbd4e75e4fa49ddce75749b0f17406f4cd0c39
nausaltech <contact@mubasherkhattak.com>
2024-05-23 14:57:22 +0500

    latest code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
           <CreditLimitsProvider />
           {children}
           <ToastContainer />
-          {/* <MicrosoftClarity /> */}
+          <MicrosoftClarity />
         </ReduxProvider>
       </AuthProvider>
     </NextThemeProvider>
