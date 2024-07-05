9995c953313f771f9f1ac3d1f2b58b0f940d288b
nausaltech <contact@mubasherkhattak.com>
2024-07-05 11:03:33 +0500

    latest code

--- src/components/public-pages/test-job-board/SuggestionCard.tsx
+++ src/components/public-pages/test-job-board/SuggestionCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SuggestionCard = ({aiResumeSuggestions}:{aiResumeSuggestions: string[]}) => {
   return (
-    <div>SuggestionCard</div>
+    <div>SuggestionCard: {aiResumeSuggestions}</div>
   )
 }
 
