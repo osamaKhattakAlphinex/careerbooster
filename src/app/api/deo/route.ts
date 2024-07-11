3f1b788946d1ae091f913de06fa82217b1a0dab7
nausaltech <contact@mubasherkhattak.com>
2024-07-11 16:50:53 +0500

    jobs fixing

--- src/app/api/deo/route.ts
+++ src/app/api/deo/route.ts
@@ -18,16 +18,11 @@ export async function GET(req: NextRequest) {
     const page = Number(url.searchParams.get("page"));
     const skip = (page - 1) * limit;
     const jobCategory = url.searchParams.get("jobCategory");
-    console.log(jobCategory);
 
     let jobs;
     let total;
     let searchCondition: any = {};
-    if (jobCategory === "Choose a Category") {
-      jobs = await Job.find({ featured: 1 });
-    } else if (jobCategory !== "Choose a Category") {
-      jobs = await Job.find({ category: jobCategory });
-    } else if (findOne) {
+    if (findOne) {
       jobs = await Job.findOne({ _id: findOne });
     } else if (jobToShow === "featured") {
       if (jobTitlequery !== "" || jobLocationquery !== "") {
