7c1ce539b2c2f5e3a97c79136579c892a320cb40
Osama Khattak <osamakhattak162@gmail.com>
2024-07-16 18:42:04 +0500

    latest

--- src/components/public-pages/ProfileResume.tsx
+++ src/components/public-pages/ProfileResume.tsx
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactPDF, {
   Document,
   Page,
   Text,
   View,
   StyleSheet,
+  Font,
 } from "@react-pdf/renderer";
-
+// Font.register("Arial")
 // Create styles
 const styles = StyleSheet.create({
   page: {
-    fontFamily: "Arial",
+    // fontFamily: ,
     fontSize: 12,
     padding: 20,
     backgroundColor: "#f4f4f4",
     color: "#333",
   },
   header: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "10px",
     textAlign: "center",
     marginBottom: 20,
   },
@@ -26,13 +30,34 @@ const styles = StyleSheet.create({
     color: "#333",
     margin: 0,
   },
+  ul: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "8px",
+    alignItems: "center",
+    width: "100%",
+  },
+  li: {
+    width: "23%",
+    display: "flex",
+    flexDirection: "row",
+    gap: "4px",
+  },
   section: {
     marginBottom: 20,
   },
+  skillSection: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    gap: "20px",
+    flexWrap: "wrap",
+  },
+
   sectionTitle: {
     textTransform: "uppercase",
     fontSize: 18,
-    marginBottom: 10,
+    marginBottom: "10px",
     borderBottom: "2px solid #333",
     paddingBottom: 5,
     marginTop: 20,
@@ -41,23 +66,51 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "#333",
   },
+  expDate: {
+    marginBottom: "10px",
+  },
+  expDesc: {
+    marginBottom: "10px",
+  },
 });
 
-function ProfileResume() {
+function ProfileResume({ userData }) {
+  console.log(userData);
   return (
     <Document>
       <Page size="A4" style={styles.page}>
         <View style={styles.header}>
-          <Text style={styles.title}>M. OSAMA AHMAD</Text>
-          <Text>
-            Delivering Excellence through Advanced Front-End Development
+          <Text style={styles.title}>
+            {userData.firstName + " " + userData.lastName}
           </Text>
-          <Text>
-            Contact: 03175216348 | Email: osamahkhalifa126@gmail.com | Address:
-            House 297 Street 61 G-13 Islamabad Pakistan 44000
+          <Text>{userData?.experience?.[0]?.jobTitle}</Text>
+
+          <Text style={styles.ul}>
+            <Text style={styles.li}>
+              contact: <Text>{userData.phone}</Text> |
+            </Text>
+            <Text style={styles.li}>
+              Email: <Text> {userData.email}</Text> |
+            </Text>
+            <Text style={styles.li}>
+              Address:{" "}
+              <Text>
+                {userData.contact.cityState +
+                  " " +
+                  userData.contact?.street +
+                  " " +
+                  userData.contact?.postalCode +
+                  " " +
+                  userData.contact.country}
+              </Text>{" "}
+              |
+            </Text>
+            <Text style={styles.li}>
+              LinkedIn: <Text>{userData.linkedin}</Text>{" "}
+            </Text>
           </Text>
         </View>
-        <View style={styles.section}>
+        {/* <View style={styles.section}>
           <Text style={styles.sectionTitle}>Executive Summary</Text>
           <Text>
             Energetic and dedicated React/Next.js Developer with expertise in
@@ -71,79 +124,63 @@ function ProfileResume() {
             technical expertise and versatile skill set towards achieving
             outstanding results in web development and design.
           </Text>
-        </View>
+        </View> */}
         <View style={styles.section}>
           <Text style={styles.sectionTitle}>Skills</Text>
-          <View>
-            <Text>• WordPress</Text>
-            <Text>• HTML/CSS</Text>
-            <Text>• Responsive Design</Text>
-            <Text>• User Interface (UI) Design</Text>
-            <Text>• Cross-Browser Compatibility</Text>
-            <Text>• Image Optimization</Text>
-            <Text>• Reusable Code</Text>
-            <Text>• Mongo DB</Text>
-            <Text>• PSD to HTML</Text>
-            <Text>• Figma to HTML</Text>
-            <Text>• Tailwind CSS</Text>
-            <Text>• Bootstrap</Text>
-            <Text>• CSS</Text>
-            <Text>• Pixel Perfect Design</Text>
+          <View style={styles.skillSection}>
+            {userData.skills.map((skill) => {
+              return <Text>• {skill}</Text>;
+            })}
           </View>
         </View>
         <View style={styles.section}>
           <Text style={styles.sectionTitle}>Work Experience</Text>
           <View>
-            <Text style={styles.jobTitle}>
-              React/Next Js Developer | careerBooster.ae | October 2022 -
-              Present
-            </Text>
-            <Text>
-              React/Next.js Development: Played a crucial role in the creation
-              of careerBooster.ae's visually appealing and user-friendly web
-              applications using React and Next.js.
-              <br />
-              Code Resusability: Implemented efficient coding practices to
-              enhance the reusability of code components, improving overall
-              development productivity.
-            </Text>
+            {userData.experience.map((experience) => {
+              return (
+                <>
+                  {" "}
+                  <Text style={styles.jobTitle}>
+                    {experience.jobTitle} |{" "}
+                    {experience.company + ", " + experience.cityState}
+                  </Text>
+                  <Text style={styles.expDate}>
+                    <Text>{experience.fromMonth}</Text>
+                    <Text> {experience.fromYear}</Text> -
+                    {experience.isContinue && <Text>&nbsp;Present</Text>}
+                    <Text> {experience.toMonth}</Text>
+                    <Text> {experience.toYear}</Text>
+                  </Text>
+                  <Text style={styles.expDesc}>{experience.description}</Text>
+                </>
+              );
+            })}
           </View>
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Education</Text>
           <View>
-            <Text style={styles.jobTitle}>
-              SEO Expert | Nasuat Technologies | January 2021 - September 2022
-            </Text>
-            <Text>
-              Client Engagement: Successfully achieved top-ranking results for
-              client websites by implementing effective SEO strategies and
-              optimization techniques.
-              <br />
-              On-Page and Off-Page SEO: Demonstrated a strong understanding of
-              both on-page and off-page SEO factors, optimizing website content
-              for better search engine rankings.
-              <br />
-              Performance Monitoring: Regularly monitored and reported on SEO
-              performance, providing clients with regular updates on SEO
-              improvements and offering tailored solutions to improve their
-              online visibility.
-            </Text>
-          </View>
-          <View>
-            <Text style={styles.jobTitle}>
-              WordPress Developer | ilmabad.net | February 2021 - April 2021
-            </Text>
-            <Text>
-              WordPress Development: Spearheaded the development of key
-              projects, including "Coderomagno.com," resulting in a 2.5%
-              increase in user engagement and 3.4% growth in site traffic.
-              <br />
-              Custom Theme Creation: Designed and implemented custom WordPress
-              themes tailored to client needs, enhancing the overall user
-              experience.
-              <br />
-              SEO Optimization: Ensured on-page SEO optimization, leading to a
-              remarkable 50% boost in search engine rankings for client websites
-              and driving higher organic traffic and conversions.
-            </Text>
+            {userData.education.map((education) => {
+              return (
+                <>
+                  {" "}
+                  <Text style={styles.jobTitle}>
+                    {education.educationLevel + " In " + education.fieldOfStudy}{" "}
+                  </Text>
+                  <Text>
+                    {education.schoolName + ", " + education.schoolLocation}{" "}
+                  </Text>
+                  <Text style={styles.expDate}>
+                    <Text>{education.fromMonth}</Text>
+                    <Text> {education.fromYear}</Text> -
+                    {education.isContinue && <Text>&nbsp;Present</Text>}
+                    <Text> {education.toMonth}</Text>
+                    <Text> {education.toYear}</Text>
+                  </Text>
+                  {/* <Text style={styles.expDesc}>{education.description}</Text> */}
+                </>
+              );
+            })}
           </View>
         </View>
       </Page>
