bb66df5d6540893298147900f4f2f7ca06ab6097
Osama Khattak <osamakhattak162@gmail.com>
2024-07-22 14:51:26 +0500

    latest

--- src/components/public-pages/Register/RegistraionForm.tsx
+++ src/components/public-pages/Register/RegistraionForm.tsx
@@ -31,7 +31,7 @@ const RegistrationForm = () => {
 
   const content = params?.get("content");
   const getProfile = params?.get("profile");
-  
+
   // Redux
 
   const formik = useFormik({
@@ -43,11 +43,18 @@ const RegistrationForm = () => {
       terms: false,
       alertConsent: false,
       file: "",
+      expectedSalary: "",
+      desiredJobTitle: "",
+      preferedLocation: "",
     },
 
     validationSchema: Yup.object({
       firstName: Yup.string().required("First name required"),
       lastName: Yup.string().required("Last name required"),
+      expectedSalary: Yup.string().required("Expected salary is required"),
+      desiredJobTitle: Yup.string().required("Desired job title is required"),
+      preferedLocation: Yup.string().required("Preferd location is required"),
+
       email: Yup.string()
         .email("Invalid Email Address")
         .required("Email address required"),
@@ -67,6 +74,9 @@ const RegistrationForm = () => {
           lastName: values.lastName,
           email: values.email,
           registeredPhone: values.phone,
+          jobTitle: values.desiredJobTitle,
+          salary: values.expectedSalary,
+          location: values.preferedLocation,
           // password: values.password,
 
           uploadedResume: {
@@ -101,7 +111,7 @@ const RegistrationForm = () => {
               setSubmitting(false);
               return setSubmittingError(res.error);
             }
-            if(getProfile === "true"){
+            if (getProfile === "true") {
               router.replace("/creating-profile?goToProfile=true");
             } else {
               router.replace("/dashboard");
@@ -517,6 +527,126 @@ const RegistrationForm = () => {
                 </p>
               )}
             </div>
+            {getProfile && (
+              <>
+                <div className="my-2 text-start">
+                  <div className="relative flex flex-wrap items-stretch w-full ">
+                    <span className="absolute flex items-center justify-center w-12 h-12 transform -translate-y-1/2 z-1000 top-1/2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-4 h-4"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                        />
+                      </svg>
+                    </span>
+
+                    <input
+                      type="text"
+                      name="expectedSalary"
+                      className="block outline-none focus:border-blue-400 dark:bg-transparent rounded-lg pr-[1.5rem] py-4 pl-[3rem] text-base w-full border-[1px] border-[#bdbfd4] bg-transparent bg-clip"
+                      placeholder="Expected Salary"
+                      onBlur={formik.handleBlur}
+                      onChange={formik.handleChange}
+                      value={formik.values.expectedSalary}
+                    />
+                  </div>
+                  {formik.touched.expectedSalary &&
+                    formik.errors.expectedSalary && (
+                      <p className="pt-3 text-red-600">
+                        {formik.touched.expectedSalary &&
+                          formik.errors.expectedSalary}
+                      </p>
+                    )}
+                </div>
+                <div className="my-2 text-start">
+                  <div className="relative flex flex-wrap items-stretch w-full ">
+                    <span className="absolute flex items-center justify-center w-12 h-12 transform -translate-y-1/2 z-1000 top-1/2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-4 h-4"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
+                        />
+                      </svg>
+                    </span>
+
+                    <input
+                      type="text"
+                      name="desiredJobTitle"
+                      className="block outline-none focus:border-blue-400 dark:bg-transparent rounded-lg pr-[1.5rem] py-4 pl-[3rem] text-base w-full border-[1px] border-[#bdbfd4] bg-transparent bg-clip"
+                      placeholder="Desired Job Title"
+                      onBlur={formik.handleBlur}
+                      onChange={formik.handleChange}
+                      value={formik.values.desiredJobTitle}
+                    />
+                  </div>
+                  {formik.touched.desiredJobTitle &&
+                    formik.errors.desiredJobTitle && (
+                      <p className="pt-3 text-red-600">
+                        {formik.touched.desiredJobTitle &&
+                          formik.errors.desiredJobTitle}
+                      </p>
+                    )}
+                </div>
+                <div className="my-2 text-start">
+                  <div className="relative flex flex-wrap items-stretch w-full ">
+                    <span className="absolute flex items-center justify-center w-12 h-12 transform -translate-y-1/2 z-1000 top-1/2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-4 h-4"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                        />
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                        />
+                      </svg>
+                    </span>
+
+                    <input
+                      type="text"
+                      name="preferedLocation"
+                      className="block outline-none focus:border-blue-400 dark:bg-transparent rounded-lg pr-[1.5rem] py-4 pl-[3rem] text-base w-full border-[1px] border-[#bdbfd4] bg-transparent bg-clip"
+                      placeholder="Prefferd Location"
+                      onBlur={formik.handleBlur}
+                      onChange={formik.handleChange}
+                      value={formik.values.preferedLocation}
+                    />
+                  </div>
+                  {formik.touched.preferedLocation &&
+                    formik.errors.preferedLocation && (
+                      <p className="pt-3 text-red-600">
+                        {formik.touched.preferedLocation &&
+                          formik.errors.preferedLocation}
+                      </p>
+                    )}
+                </div>
+              </>
+            )}
             {/* <div className="my-2 text-start">
               <div className="relative flex flex-wrap items-stretch w-full">
                 <span className="absolute flex items-center justify-center w-12 h-12 transform -translate-y-1/2 z-1000 top-1/2">
