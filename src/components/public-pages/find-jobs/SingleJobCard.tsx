242f6e2ba6b3945f3bbd5fcca294cc846bb1ac0e
nausaltech <buttusamarwp@gmail.com>
2024-07-30 11:51:00 +0500

    setting the job posting and easy apply option also added now candidates profile sent to employer

--- src/components/public-pages/find-jobs/SingleJobCard.tsx
+++ src/components/public-pages/find-jobs/SingleJobCard.tsx
@@ -1,28 +1,66 @@
-import { eyeIcon } from "@/helpers/iconsProvider";
+import { eyeIcon, sparkleIcon } from "@/helpers/iconsProvider";
+import {
+  showErrorToast,
+  showInfoToast,
+  showSuccessToast,
+} from "@/helpers/toast";
+import { RootState } from "@/store/store";
+import axios from "axios";
 import Link from "next/link";
+import { useSelector } from "react-redux";
 type Props = {
   jobTitle: string;
   location: string;
   employer: string;
   jobDescription: string;
-  viewjoblink?: any;
   applyJobLink: any;
   jobId: string;
-  category: string;
+  addedBy: string;
+  noOfProposals: number;
+  applicationProfiles: string[];
 };
 export default function SinglejobCard({
-  category,
   jobTitle,
   location,
   employer,
   jobDescription,
-  viewjoblink,
   applyJobLink,
   jobId,
+  addedBy,
+  noOfProposals,
+  applicationProfiles,
 }: Props) {
+  const userData = useSelector((state: RootState) => state.userData);
+  const jobProfileToDb = () => {
+    if (applicationProfiles.includes(`/profile/${userData._id}`)) {
+      showInfoToast("Already Applied to this Job");
+      return;
+    }
+    if (userData._id && userData.role !== "employer") {
+      axios
+        .put(`/api/deo?jobId=${jobId}`, {
+          applicationProfiles: [
+            ...applicationProfiles,
+            `/profile/${userData._id}`,
+          ],
+          noOfProposals: noOfProposals + 1,
+        })
+        .then((response) => {
+          (applicationProfiles = [
+            ...applicationProfiles,
+            `/profile/${userData._id}`,
+          ]),
+            (noOfProposals = noOfProposals + 1);
+          showSuccessToast("Applied Successfully");
+        })
+        .catch((err) => console.log("Error occured", err));
+    } else {
+      showErrorToast("Please Login as User to Apply");
+    }
+  };
   return (
     <div className="card md:flex md:flex-row xs:flex-col  dark:border dark:border-gray-100 dark:bg-black bg-gray-100  md:mx-14 md:px-8 py-10 rounded-md mt-4 shadow-lg xs:mx-4 xs:px-2">
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-6 w-full">
         <div className="md:flex md:flex-row xs:flex-col gap-6 items-center">
           {/* {category} */}
           <h2 className="dark:text-gray-100 text-gray-950 text-xl font-bold xs:mb-3">
@@ -114,27 +152,51 @@ export default function SinglejobCard({
             {eyeIcon}
             View Job
           </Link>
-          <Link
-            href={applyJobLink}
-            target="_blank"
-            className="flex border-2 items-center gap-2 w-fit rounded-md dark:bg-gray-100 dark:text-gray-950 bg-gray-950 text-gray-100 font-semibold px-3 md:text-base xs:text-sm py-1 hover:bg-gray-100 hover:text-gray-950 dark:hover:bg-transparent dark:hover:text-gray-100 "
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-4 h-4"
+          {applyJobLink && (
+            <Link
+              href={applyJobLink}
+              target="_blank"
+              className="flex border-2 items-center gap-2 w-fit rounded-md dark:bg-gray-100 dark:text-gray-950 bg-gray-950 text-gray-100 font-semibold px-3 md:text-base xs:text-sm py-1 hover:bg-gray-100 hover:text-gray-950 dark:hover:bg-transparent dark:hover:text-gray-100 "
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
-              />
-            </svg>
-            Apply Now
-          </Link>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
+                />
+              </svg>
+              Apply Now
+            </Link>
+          )}
+          {addedBy === "employer" && (
+            <button
+              onClick={jobProfileToDb}
+              className="flex border-2 items-center gap-2 w-fit rounded-md dark:bg-gray-100 dark:text-gray-950 bg-gray-950 text-gray-100 font-semibold px-3 md:text-base xs:text-sm py-1 hover:bg-gray-100 hover:text-gray-950 dark:hover:bg-transparent dark:hover:text-gray-100 "
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+                />
+              </svg>
+              Easy Apply
+            </button>
+          )}
         </div>
       </div>
     </div>
